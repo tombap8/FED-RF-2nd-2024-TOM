c636001f58961d635b6153deafddcb57c854bfb6
Tom B. Parker <tombap8@hotmail.com>
2024-03-22 15:54:12 +0900

    ss

--- 001.HTML학습/07.ShoppingMall_PJ/js/main0.js
+++ 001.HTML학습/07.ShoppingMall_PJ/js/main0.js
@@ -42,12 +42,16 @@ addEvt(window,"DOMContentLoaded", loadFn);
 ******************************************/
 function loadFn() {
     console.log("로딩완료!");
+    
 
     // 이동버튼 대상:  .abtn
     const abtn = qsa('.abtn');
     // 변경대상 : #slide
     const slide = qs('#slide');
     // console.log(abtn,slide);
+
+    // 오른쪽 버튼 처음에 숨기기
+    abtn[0].style.display = 'none';
 
     // 슬라이드 순번 전역변수
     let snum = 0;
@@ -71,7 +75,12 @@ function loadFn() {
      ******************************************/
     function goSlide(){
         // 1.오른쪽 버튼인 .ab2인가?
-        let isRbtn = this.classList.contains('ab2');
+        let isRbtn = 
+        this.classList.contains('ab2');
+        // [classList 객체의 contains() 메서드]
+        // -> 해당요소의 특정 클래스인지 여부를 리턴함
+        // 해당클래스가 있으면 true, 없으면 false
+
 
         // 함수호출확인
         console.log('나 슬라이드야~!',this,isRbtn);
@@ -80,6 +89,31 @@ function loadFn() {
         // 2. 오른쪽버튼이면 ++, 아니면 --
         // 슬라이드 순번 증감
         isRbtn? snum++ : snum--;
+
+        // 3. 한계값 설정하기
+        // 한계값일때 각 버튼 숨기기
+        // 3-1. 오른쪽 버튼일 경우 0보다 작으면 숨기기
+        // snum은 0으로 고정!
+        if(snum<0){
+            snum = 0;
+        } /// if ///
+        // snum은 4로 고정!
+        else if(snum>4){
+            snum = 4;
+        } /// else if ///
+        
+        // 4. 마지막 구역에서 버튼 숨기기
+        if(snum === 0 || snum === 4){
+            // 해당버튼 숨기기
+            this.style.display = 'none';            
+        } /// if ///
+        else{
+            // 버튼 다시 보이기
+            for(let x of abtn){
+                x.style.display = 'block'; 
+            } /// for of ///
+
+        } /// else ///
         
 
        console.log('이동%:',(-100*snum)+'%');
