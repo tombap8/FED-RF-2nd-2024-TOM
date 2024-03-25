1a157bb4fd65da1c6196718971abd8d60be53a31
Tom B. Parker <tombap8@hotmail.com>
2024-03-25 12:18:03 +0900

    Update main.js

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -15,39 +15,43 @@
 ***********************************/
 // 1. 대상선정
 // 1-1. 이벤트 대상 : .poster-menu a
-const pMenu = document.querySelectorAll('.poster-menu a');
+const pMenu = document.querySelectorAll(".poster-menu a");
 // 1-2. 변경 대상 : #ifr
-const ifr = document.querySelector('#ifr');
+const ifr = document.querySelector("#ifr");
 
-console.log('대상:',pMenu,ifr);
+console.log("대상:", pMenu, ifr);
 
 // 2. 영화아이디 정보 객체로 구성하기
 const movieId = {
-    "파묘":"rjW9E1BR_30",
-    "이프온리":"WGFapljXfnU",
-    "웡카":"Bldf9SWRPFM",
-    "시민덕희":"Qwhmn6Dimsg",
-    "소풍":"7VHsScXQyw0",
-    "듄:파트2":"4JElrZ1WB40",
-}
+  파묘: "rjW9E1BR_30",
+  이프온리: "WGFapljXfnU",
+  웡카: "Bldf9SWRPFM",
+  시민덕희: "Qwhmn6Dimsg",
+  소풍: "7VHsScXQyw0",
+  "듄:파트2": "4JElrZ1WB40",
+};
 
 // 3. 이벤트 설정 및 기능구현
 // 포스터 버튼에 forEach()메서드로 순회한다!
-pMenu.forEach((ele)=>{
-    ele.onclick = ()=>{
-        // 1.클릭된 a요소를 구분하기 위해
-        // 하위 img 포스터의 alt 속성 읽어오기
-        // 속성읽기 내장함수 : getAttribute(속성명)
-        let txt = ele.querySelector('img').getAttribute('alt');
-        console.log('나클릭!',txt);
+pMenu.forEach((ele) => {
+  ele.onclick = () => {
+    // 1.클릭된 a요소를 구분하기 위해
+    // 하위 img 포스터의 alt 속성 읽어오기
+    // 속성읽기 내장함수 : getAttribute(속성명)
+    let txt = ele.querySelector("img").getAttribute("alt");
+    console.log("나클릭!", txt);
 
-        // 2. 아이프레임 src변경하기
-        // 속성변경 JS내장함수
-        // -> setAttribute(속성명,값)
-        // 대상: 아이프레임 (#ifr->ifr변수)
-        ifr.setAttribute('src',
-        `https://www.youtube.com/embed/${movieId[txt]}?autoplay=1`);
-
-    }; /// click함수 ////
+    // 2. 아이프레임 src변경하기
+    // 속성변경 JS내장함수
+    // -> setAttribute(속성명,값)
+    // 대상: 아이프레임 (#ifr->ifr변수)
+    // 영화아이디값 -> movieId객체
+    // 객체호출법 : movieId[영화이름속성명]
+    // 영화이름속성명은 txt변수에 할당되어 있음
+    // 객체호출코드 : movieId[txt]
+    ifr.setAttribute(
+      "src",
+      `https://www.youtube.com/embed/${movieId[txt]}?autoplay=1`
+    );
+  }; /// click함수 ////
 }); //// forEach /////
-
