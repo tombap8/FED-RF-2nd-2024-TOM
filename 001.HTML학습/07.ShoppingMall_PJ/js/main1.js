f89feefd6c7a363148bc3070810540fbaf66e2c2
Tom B. Parker <tombap8@hotmail.com>
2024-03-26 15:46:30 +0900

    ss

--- 001.HTML학습/07.ShoppingMall_PJ/js/main1.js
+++ 001.HTML학습/07.ShoppingMall_PJ/js/main1.js
@@ -59,7 +59,33 @@ function loadFn() {
   const abtn = qsa(".abtn");
   // 변경대상 : #slide
   const slide = qs("#slide");
+  // 블릿버튼 : .indic
+  let indic = document.querySelector(".indic");
   // console.log(abtn,slide);
+
+  //////////// 초기셋팅하기 ////////
+  // 5개의 슬라이드와 블릿을 만들어준다!
+  for (let i = 0; i < 5; i++) {
+    // 슬라이드 넣기
+    slide.innerHTML += `
+    <li data-seq="${i}">
+        <img 
+        src="images/slide0${i + 1}.jpg"         
+        alt="slide">
+    </li>    
+    `;
+    // 블릿 넣기
+    indic.innerHTML += `
+    <li ${i === 0 ? 'class="on"' : ""}>
+        <img src="images/dot1.png" alt="흰색">
+        <img src="images/dot2.png" alt="회색">
+    </li>
+    `;
+  } ////// for ////////
+
+  // li를 생성한 후 그 li다시 수집한다!
+  // 블릿의 li까지 수집! indic 변수
+  indic = document.querySelectorAll('.indic li');
 
   // 슬라이드 순번 전역변수
   let snum = 0;
@@ -85,14 +111,13 @@ function loadFn() {
      기능: 슬라이드 이동
      ******************************************/
   function goSlide() {
-
     // 광클금지 설정하기 ///////////
     // 클릭신호를 막아서 못들어오게 하고
     // 일정시간후 다시 열어준다!
-    if(prot) return; // 돌아가!(함수나감!)
-    prot=true; // 잠금! (뒤의호출막기!)
+    if (prot) return; // 돌아가!(함수나감!)
+    prot = true; // 잠금! (뒤의호출막기!)
     setTimeout(() => {
-        prot=false; // 0.6초후 해제!
+      prot = false; // 0.6초후 해제!
     }, 600);
     /////////////////////////////////////
 
@@ -116,13 +141,12 @@ function loadFn() {
       // (2)이동하는 시간 0.6초간 기다림!
       setTimeout(() => {
         // (2-1) 맨앞 li 맨뒤로 이동
-        slide.appendChild(
-            slide.querySelectorAll("li")[0]);
+        slide.appendChild(slide.querySelectorAll("li")[0]);
         // 슬라이드 left 값이 -100% 이므로
         // (2-2) left값을 0으로 변경
-        slide.style.left = '0';
+        slide.style.left = "0";
         // (2-3) left 트랜지션 없애기
-        slide.style.transition = 'none';
+        slide.style.transition = "none";
       }, 600);
 
       // 맨 앞li 맨뒤로 이동하기
@@ -134,44 +158,65 @@ function loadFn() {
     } //// if ////
 
     // 2-2.왼쪽 버튼일 경우 ////
-    else{
-        // 하위 li수집
-        let list = slide.querySelectorAll('li');
-        // (1)맨뒤 li 맨앞으로 이동하기
-        // 놈놈놈 시리즈!
-        // insertBefore(넣을놈,넣을놈전놈)
-        // insertBefore(맨뒤li,맨앞li)
-        slide.insertBefore(
-            list[list.length-1],list[0]);
-        
-        // (2) left 값을 -100%로 변경하여
-        // 맨뒤 li가 맨앞으로 온것을 숨긴다!
-        // 왼쪽에서 슬라이드 들어올 준비!!!
-        slide.style.left = '-100%';
-        // 트랜지션이 한번 버튼클릭후 생기므로 없애줌
-        slide.style.transition = 'none';
+    else {
+      // 하위 li수집
+      let list = slide.querySelectorAll("li");
+      // (1)맨뒤 li 맨앞으로 이동하기
+      // 놈놈놈 시리즈!
+      // insertBefore(넣을놈,넣을놈전놈)
+      // insertBefore(맨뒤li,맨앞li)
+      slide.insertBefore(list[list.length - 1], list[0]);
 
-        //////////////////////////////////
-        // 같은 left 값을 변경하기 때문에
-        // 코드 처리구역을 분리하여준다!
-        // 이때 사용되는 메서드는  setTimeout()!
-        // 시간차는 어쪄죠? 0을 줘도 코드를
-        // 분리하여 처리하므로 동시처리가 아니고
-        // 비동기처리하기 때문에 코드가 잘 작동한다!
-        setTimeout(() => {
-            // (3) left 값을 0으로 트랜지션하여 들어옴
-            slide.style.left = '0';
-            slide.style.transition = 
-            ".6s ease-in-out";
-            
-        }, 0);
+      // (2) left 값을 -100%로 변경하여
+      // 맨뒤 li가 맨앞으로 온것을 숨긴다!
+      // 왼쪽에서 슬라이드 들어올 준비!!!
+      slide.style.left = "-100%";
+      // 트랜지션이 한번 버튼클릭후 생기므로 없애줌
+      slide.style.transition = "none";
 
-
-
-
+      //////////////////////////////////
+      // 같은 left 값을 변경하기 때문에
+      // 코드 처리구역을 분리하여준다!
+      // 이때 사용되는 메서드는  setTimeout()!
+      // 시간차는 어쪄죠? 0을 줘도 코드를
+      // 분리하여 처리하므로 동시처리가 아니고
+      // 비동기처리하기 때문에 코드가 잘 작동한다!
+      setTimeout(() => {
+        // (3) left 값을 0으로 트랜지션하여 들어옴
+        slide.style.left = "0";
+        slide.style.transition = ".6s ease-in-out";
+      }, 0);
     } /// else ///
 
+    // 3.블릿을 위해 읽어올 슬라이드 순번 구하기
+    // 현재순번은 몇번째 슬라이드의 
+    // data-seq속성값이다!
+    // 오른쪽버튼은 
+    // 이동후 잘라내므로 두번째순번[1]
+    // 왼쪽버튼은 먼저 앞에 붙이고 
+    // 이동하므로 첫번째순번[0]
+    let seq = 
+    slide.querySelectorAll('li')[isRbtn?1:0]
+    .getAttribute('data-seq');
+    console.log('블릿이 읽어올 슬순번:',seq,
+    '/데이터형:',typeof seq);
+    // string - 문자형, number - 숫자형
 
+    // 4. 블릿변경하기 ///////////
+    // 모든 클래스 on지우기+현재 순번 클래스 넣기
+    indic.forEach((ele,idx)=>{
+        // ele - 각각의 li, idx - 각각의 순번
+        if(idx==seq){ // 현재순번 on넣기
+            // ==으로 비교해야 결과가 나옴
+            // data-seq 속성은 문자형숫자이므로!
+            // ===은 형까지 비교하기때문에 안나옴!
+            ele.classList.add('on');
+        } /// if ///
+        else{ // 나머지는 on빼기
+            ele.classList.remove('on');
+        } /// else ///
+
+       }); ///// forEach /////
 
 
   } ///////////// goSlide 함수 ////////////////
