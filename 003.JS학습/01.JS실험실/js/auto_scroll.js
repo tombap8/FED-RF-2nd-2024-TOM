cb63859910ea0b1e557aafe829f2c6bd13e67c01
Tom B. Parker <tombap8@hotmail.com>
2024-03-26 12:20:36 +0900

    ss

--- 003.JS학습/01.JS실험실/js/auto_scroll.js
+++ 003.JS학습/01.JS실험실/js/auto_scroll.js
@@ -2,7 +2,8 @@
 
 /********************************************** 
     [ 자동스크롤 기능정의 ]
-    1. 스크롤바가 없는 상태에서 마우스 휠 작동시
+    1. 스크롤바가 없는 상태에서 또는 스크롤기능을 
+    막아놓은 상태에서 마우스 휠 작동시
     아래와 같이 기능구현됨
     (1) 휠 내림 : 다음페이지로 이동
     (2) 휠 올림 : 이전페이지로 이동
@@ -12,6 +13,82 @@
     [ 자동스크롤 이벤트 ]
     -> wheel 이벤트
     -> 마우스 휠 작동시 발생!
-    (이전 이벤트명: mousewheel / DOMMouseScroll(파이어폭스))
+    (이전 이벤트명: 
+        mousewheel / DOMMouseScroll(파이어폭스))
+    (주의: wheel이벤트는 scroll이벤트와는 다름!
+        마우스의 바퀴를 돌릴때 발생함!)
 **********************************************/
 
+// 1.  전역변수 설정하기 /////////////
+// 1-1. 페이지변수
+let pgNum = 0;
+// 1-2. 휠상태변수(true는 막기/false는 통과)
+let stsWheel = false;
+// 1-3. .page클래스 요소
+const elePage = 
+document.querySelectorAll('.page');
+// 1-4. 전체페이지수
+const totalCnt = elePage.length;
+console.log('대상:',elePage,totalCnt,'개');
+
+// 2. 이벤트 등록하기 /////////////
+// 대상: window
+// 전체 페이지 휠 이벤트의 대상은 window다! 
+// 휠이벤트설정
+window.addEventListener('wheel',wheelFn,{passive:false});
+/* 
+    [ window / document / body 세가지는
+    기본막기불가 설정되어있음! ]
+    -> 이벤트 등록시 패지스모드가 true로 설정됨
+    셋팅방법:
+    요소.addEventListener(이벤트명,함수,{passive:true})
+    -> 기존엔  passive:false 였는데 이것의
+    기본값이 true로 변경됨. 의미는 기본기능막기
+    못하게 설정됨!
+
+    우리가 변경하여 사용해야함!
+    요소.addEventListener(이벤트명,함수,{passive:false})
+*/
+
+
+// 3. 함수 구현하기 ////////////////////
+
+/*************************************** 
+    함수명: wheelFn
+    기능 : 마우스 휠 작동시 페이지이동
+***************************************/
+function wheelFn(e){ // 이벤트전달변수(자동)
+    // 함수호출확인!
+    console.log('휠~~~!');
+
+    // 광휠금지장치 //////
+    if(stsWheel) return; // 돌아가!
+    stsWheel = true; // 잠금!
+    setTimeout(() => {
+        stsWheel = false; // 잠금해제!
+    }, 500);
+
+    // 우리는 휠 기본기능을 막고
+    // 자동으로 스크롤을 하나씩 되게 할 것이다!
+    e.preventDefault();
+    // -> passive:false설정해야함!왜?window니까!
+
+    // 1. 휠방향 알아내기 /////
+    let delta = e.wheelDelta;
+    // 휠델타는 이벤트 객체에서 리턴해주는
+    // 방향, 이동거리 등의 정보값이다!
+    console.log('델타값:', delta);
+    // -> 마이너스가 아랫방향임!
+
+    // 2. 방향별 분기하기 ///////
+    if(delta<0){
+        // 아랫페이지로 가야하니까 페이지번호증가
+        pgNum++;
+    } /// if ///
+    else{
+        // 반대는 윗방향이니까 페이지번호감소
+        pgNum--;
+    } /// else ///
+    console.log('pgNum:',pgNum);
+} /////////// wheelFn 함수 ////////////////
+///////////////////////////////////////////
