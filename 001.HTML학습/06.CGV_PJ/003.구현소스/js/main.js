7f095b61b063783cc729065200f3d8e824d99ee9
Tom B. Parker <tombap8@hotmail.com>
2024-04-01 10:57:31 +0900

    Update main.js

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -33,7 +33,8 @@ const movieId = {
 
 // 3. 이벤트 설정 및 기능구현
 // 포스터 버튼에 forEach()메서드로 순회한다!
-pMenu.forEach((ele) => {
+pMenu.forEach((ele,idx) => { 
+  // ele - 각 a요소 / idx - 각 요소의 순번
   ele.onclick = () => {
     // 1.클릭된 a요소를 구분하기 위해
     // 하위 img 포스터의 alt 속성 읽어오기
@@ -53,5 +54,21 @@ pMenu.forEach((ele) => {
       "src",
       `https://www.youtube.com/embed/${movieId[txt]}?autoplay=1`
     );
+
+    // 3. 클릭된 a의 부모인 li에 클래스 on넣기
+    // forEach메서드 사용해보자!
+    pMenu.forEach((x,i)=>{ // x - a요소 / i - 순번
+      // x.parentElement는 a요소 상위부모 li요소
+      if(i===idx){ // 해당순번은 on넣기
+        x.parentElement.classList.add("on");
+      } /// if ///
+      else{ // 나머지는 on빼기
+        x.parentElement.classList.remove("on");
+      } /// else ///
+    }); ////// forEach //////
+
+
+
+
   }; /// click함수 ////
 }); //// forEach /////
