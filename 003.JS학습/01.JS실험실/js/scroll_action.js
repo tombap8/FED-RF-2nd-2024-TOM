f2dd6d7c5a735efe840ec56a84d7b1736428b983
Tom B. Parker <tombap8@hotmail.com>
2024-04-02 15:59:40 +0900

    Update scroll_action.js

--- 003.JS학습/01.JS실험실/js/scroll_action.js
+++ 003.JS학습/01.JS실험실/js/scroll_action.js
@@ -70,13 +70,41 @@ myFn.addEvt(window,'scroll',showIt);
 // 3. 함수 만들기 ///////////////
 // 3-1. 스크롤 등장액션 함수
 function showIt() {
-    let temp = scAct[0].getBoundingClientRect().top;
+
+    // 클래스 on넣기 함수 호출하기
+    // for of 문 호출
+    for(let x of scAct) addOn(x);
+
+
+    // let pos = myFn.getBCR(scAct[0]);
+    // let pos2 = myFn.getBCR(scAct[1]);
+    // let pos3 = myFn.getBCR(scAct[2]);
     // 함수호출확인
-    console.log('첫번째대상위치:',temp);
+    // console.log('첫번째대상위치:',pos);
+
+    // if(pos<500) scAct[0].classList.add('on');
+    // if(pos2<500) scAct[1].classList.add('on');
+    // if(pos3<500) scAct[2].classList.add('on');
 
 
 
 } /////////// showIt 함수 /////////////
+
+// 스크롤 등장 기준설정 : 화면의 2/3
+const CRITERIA = window.innerHeight / 3 * 2;
+console.log('기준값:',CRITERIA);
+
+//// [ 클래스 on 넣기 함수 ] ///////////
+function addOn(ele){ // ele - 대상요소
+    // 바운딩값 구하기
+    let bcrVal = myFn.getBCR(ele);
+
+    // 기준값보다 작을때 등장
+    if(bcrVal < CRITERIA) ele.classList.add('on');
+    // 기준값보다 크면 원상복귀(숨김-on빼기)
+    else ele.classList.remove('on');
+
+} ///////////// addOn 함수 //////////////
 
 // [ 글자등장 셋팅하기 ] /////////////////
 // 글자등장함수 //////
