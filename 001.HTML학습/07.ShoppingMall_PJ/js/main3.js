c775ae9ba249b11df59c358f6ddd6e22e19bf31e
Tom B. Parker <tombap8@hotmail.com>
2024-04-02 11:20:23 +0900

    Update main3.js

--- 001.HTML학습/07.ShoppingMall_PJ/js/main3.js
+++ 001.HTML학습/07.ShoppingMall_PJ/js/main3.js
@@ -42,15 +42,19 @@ function loadFn() {
   // 변경대상 : #slide
   const slide = qs("#slide");
   // 블릿버튼 : .indic
-  let indic = document.querySelector(".indic");
+  let indic = qs(".indic");
   // console.log(abtn,slide);
+
+  // 슬라이드(블릿)개수 상수로 셋팅하기!
+  // 상수는 대문자로 쓰고 단어구분은 언더바로함!
+  const SLIDE_CNT = 5;
 
   //////////// 초기셋팅하기 ////////
   // 5개의 슬라이드와 블릿을 만들어준다!
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < SLIDE_CNT; i++) {
     // 슬라이드 넣기
     slide.innerHTML += `
-    <li data-seq="${i}">
+    <li ${i === 0 ? 'class="on"' : ""}>
         <img 
         src="images/slide0${i + 1}.jpg"         
         alt="slide">
@@ -67,9 +71,45 @@ function loadFn() {
 
   // li를 생성한 후 그 li다시 수집한다!
   // 블릿의 li까지 수집! indic 변수
-  indic = document.querySelectorAll('.indic li');
+  indic = document.querySelectorAll(".indic li");
 
   // 슬라이드 순번 전역변수
   let snum = 0;
 
+  // 1. 이벤트 연결 설정하기 //////
+  // 대상: .abtn
+  abtn.forEach((ele) => {
+    addEvt(ele, "click", goSlide);
+  });
+  // addEvt(대상,이벤트,함수)
+
+  // 2. 이벤트 처리 함수 만들기 //////
+  // ((처리순서))
+  // (1) 오른쪽버튼이면 전역슬라이드 변수 snum++
+  // (2) 왼쪽버튼이면 전역슬라이드 변수 snum--
+  // (3) 이때 한계값을 체크하여 순환되게 함
+  // -> 끝번호 뒤는 첫번호, 첫번호 앞은 끝번호
+  // (4) 해당순번의 슬라이드에 클래스 on 넣기
+  // -> 나머지 슬라이드는 on 제거하기 (외부함수구성)
+  // (5) 블릿표시자도 슬라이드가 같은 순번에
+  // 클래스 on넣고 나머지는 빼준다 (외부함수구성)
+  // (6) 자동넘김 구성함수를 호출하여 인터발호출작동
+  // -> 버튼 클릭시 인터발 지우기
+  // -> 일정시간뒤 다시 인터발 작동
+  function goSlide() {
+    // 1.오른쪽버튼 여부
+    let isRbtn = this.classList.contains("ab2");
+    // 호출확인
+    // console.log('오른쪽버튼이니?',isRbtn,this);
+
+    // 2.버튼에 따른 전역 슬라이드 번호 증감하기
+    // (1) 오른쪽버튼일 경우 증가
+    // -> 한계설정: snum이 개수-1과 같으면 첫번호 0
+    if (isRbtn) snum === SLIDE_CNT - 1 ? (snum = 0) : snum++;
+    // (2) 왼쪽버튼일 경우 감소
+    // -> 한계설정: snum이 0이면 마지막순번
+    else snum === 0 ? (snum = SLIDE_CNT - 1) : snum--;
+
+    console.log("snum: " + snum);
+  } /////////// goSlide 함수 /////////////
 } /////////////// loadFn 함수 //////////////
