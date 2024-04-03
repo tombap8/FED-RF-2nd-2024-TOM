e075494407e0ab1bdb348cb91153eef71ff0ca59
Tom B. Parker <tombap8@hotmail.com>
2024-04-03 12:13:06 +0900

    ss

---
+++ 001.HTML학습/05.Nav_PJ/js/drag_banner.js
@@ -0,0 +1,1 @@
+// JS실험실 : 10.드래그배너 JS - drag_banner.js 와 연결!
---
+++ 001.HTML학습/05.Nav_PJ/js/nav06.js
@@ -0,0 +1,13 @@
+// 네비게이션 유형6 JS - nav06.js
+// 가로네비 서브별 드롭다운 전체창
+
+const myFn = {
+    // 요소선택함수 ////////
+    qs: (x) => document.querySelector(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsa: (x) => document.querySelectorAll(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+  
+    // 이벤트셋팅함수
+    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+  }; /////// myFn 객체 /////////////
