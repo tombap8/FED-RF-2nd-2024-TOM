1d1825df4b93594af64a031ef0fbd0ff4fb110d2
Tom B. Parker <tombap8@hotmail.com>
2024-04-05 11:48:29 +0900

    Update main.js

--- 003.JS학습/04.JS게임/js/main.js
+++ 003.JS학습/04.JS게임/js/main.js
@@ -6,17 +6,17 @@ import myFn from "./dom.js";
 // 메시지 제이슨 파일 불러오기
 // -> 어서써 타입 제이슨!!!
 // -> 내가 지은 변수명으로 import후 맨끝에 assert {type:'json'}씀
-import msgTxt from "./data_racing.json" assert { type: "json" };
+// import msgTxt from "./data_racing.json" assert { type: "json" };
 
 // 'assert' is deprecated in import statements and support will be removed
 // in V8 v12.6 and Chrome 126; use 'with' instead
 // -> assert 키워드 지원중단됨(사용은가능). 대신 with 키워드 권장!
 // -> 같이써 타입제이슨! with {type:'json'}
 // -> 브라우저 지원버전이 너무 최신이라 지금은 assert를 사용하자!
-// import msgTxt from './data_racing.json' with {type:'json'};
+import msgTxt from './data_racing.json' with {type:'json'};
 
 // 불러온 것 확인
-console.log(myFn, msgTxt);
+// console.log(myFn, msgTxt);
 
 /********************************************** 
             [ 게임 기능정의 ]
@@ -82,13 +82,15 @@ function goGame() {
 
   // 2. 버튼별 기능분기하기 ////
   if(btxt === '토끼출발'){
-    goR1();
-
+    goR1(); // 인터발호출함수
   } /// if ///
   else if(btxt === '거북출발'){
     // 거북의 설정된 값만큼 이동하기
     t1pos += T1_NUM;
     t1.style.left = t1pos + 'px';
+    
+    // 토끼 자동호출
+    goR1();
 
   } /// else if ///
   else if(btxt === '처음으로'){
@@ -106,9 +108,18 @@ function goGame() {
 // 인터발지우기용 변수
 let autoI;
 function goR1(){
-    setInterval(() => {
-        r1.style.left = ++r1pos + 'px';
-    }, 10);
+    // 호출이 한번만 되도록 autoI가 할당전엔 undefined
+    // 이므로 if문에서 false 처리됨! 이것이 이용하자!
+    if(!autoI){ // false일때만 들어감(할당전에만)
+        console.log('토끼 인터발!!!',level.value);
+        autoI = setInterval(() => {
+            r1.style.left = ++r1pos + 'px';
+        }, level.value);
+        // level.value는 선택박스의 선택된 값이다!
+        // 원래 option요소의 value값은 문자형이므로
+        // 숫자여도 숫자형으로 형변환해야하지만
+        // 요즘 브라우저는 자동형변환 해준다!
+    } //// if ////
 
 } ///////// goR1함수 //////////////////
 
