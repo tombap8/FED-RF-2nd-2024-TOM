dd0273efa75513ea7ba01ccc063043dbfe812906
Tom B. Parker <tombap8@hotmail.com>
2024-04-08 15:02:01 +0900

    ss

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/data/dkb_data.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/data/dkb_data.js
@@ -257,6 +257,64 @@ const previewData = [
   },
 ];
 
+// 2. 현장포토 리스트 데이터
+/************************************* 
+    [ 데이터항목 : ]
+    1.순번(기본키) : idx
+    2.제목 : title
+    3.이미지명 : imgName
+    4.날짜 : date
+*************************************/
+const liveData = [
+  {
+    idx: "1",
+    title: "다 드립니다 ♡",
+    imgName: "live_photo7",
+    date: "2023-09-08",
+  },
+  {
+    idx: "2",
+    title: "비하인드 짤털이~",
+    imgName: "live_photo6",
+    date: "2023-09-08",
+  },
+  {
+    idx: "3",
+    title: "꿀 떨어지는 도깨비 내외",
+    imgName: "live_photo5",
+    date: "2023-09-08",
+  },
+  {
+    idx: "4",
+    title: "사랑이 넘치네요",
+    imgName: "live_photo8",
+    date: "2023-09-08",
+  },
+  {
+    idx: "5",
+    title: "덕화에게서 낯선 향기가 난다",
+    imgName: "live_photo4",
+    date: "2023-09-08",
+  },
+  {
+    idx: "6",
+    title: "안녕하세요",
+    imgName: "live_photo3",
+    date: "2023-09-08",
+  },
+  {
+    idx: "7",
+    title: "시청자여러분",
+    imgName: "live_photo2",
+    date: "2023-09-08",
+  },
+  {
+    idx: "8",
+    title: "도깨비를마니마니",
+    imgName: "live_photo1",
+    date: "2023-09-08",
+  },
+]; /////////// liveData 배열 /////////
 
 // 데이터 공개하기 //////
-export {previewData};
+export { previewData, liveData };
