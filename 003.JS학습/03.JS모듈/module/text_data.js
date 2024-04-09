8d07346ed07aec6201374bb4fd2e7d6f3b1aa16a
Tom B. Parker <tombap8@hotmail.com>
2024-04-09 10:07:22 +0900

    ss

--- 003.JS학습/03.JS모듈/module/text_data.js
+++ 003.JS학습/03.JS모듈/module/text_data.js
@@ -53,7 +53,13 @@ const mvData = [
     ],
 ];
 
-export {mTitle,sTitle,personInfo,mvData};
+// 객체 예시
+const myObj = {name:'수현',age:34};
+
+// 함수 예시
+const myFunc = x => `나는 ${x}다!`;
+
+export {mTitle,sTitle,personInfo,mvData,myObj,myFunc};
 
 
 /******************************************** 
