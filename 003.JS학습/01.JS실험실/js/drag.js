99e0145fa126ac5d9bac5339814e5518f97b5f18
Tom B. Parker <tombap8@hotmail.com>
2024-04-18 10:41:43 +0900

    ss

--- 003.JS학습/01.JS실험실/js/drag.js
+++ 003.JS학습/01.JS실험실/js/drag.js
@@ -49,27 +49,39 @@ function setDrag(clsName) {
   // 2. 드래그 함수 호출한다!
   // HTML 컬렉션 이므로 forEach메서드로 호출
   // forEach((요소,순번,전체)=>{})
-  ele.forEach((x,y,z) => goDrag(x,z));
-  // z는 전체 요소집합 컬렉션임(z-index초기화로 필요함!)
+  ele.forEach((x) => goDrag(x));
+  // z는 전체 요소집합 컬렉션임(z-index초기화로 필요함!)-안함!
 } /////////// setDrag 함수 ////////////////
+
+// z-index 공통관리 변수(전역변수)
+let zNum = 0;
 
 /******************************************* 
     [ 드래그 다중적용 함수 만들기 ]
     함수명 : goDrag
     기능 : 다중 드래그 기능 적용
+
+    수정필요사항 체크
+    1) 드래그 시 위치이동 안되는 버그
+    (원인:top,left초기값 셋팅안될경우 에러)
+    2) z-index 초기화로 인한 순서변경 어색
+
 *******************************************/
-function goDrag(ele, coll) {
+function goDrag(ele) {
   // ele - 호출시 보내준 대상을 받는 변수
   // -> 하나씩 전달된 드래그 대상 요소임!
-  // coll - 드래그 요소 전체 컬렉션을 받는 변수
-  // -> 마우스 다운시 z-index 대상 1로 만들때 
-  //    다른요소는 0 변경시 사용
-  console.log(ele,coll);
+  console.log(ele);
 
   // 드래그 적용 대상 및 이벤트 설정하기 ////
   // 1. 대상선정 : 보내준 대상 HTML컬렉션
   const dtg = ele;
   // const dtg = mFn.qs('.dtg2');
+
+  // 드래그할 대상의 CSS 기본값을 셋팅한다!
+  // 필수 셋팅요소는 position:relative / top:0 / left:0
+  dtg.style.position = "relative";
+  dtg.style.top = "0";
+  dtg.style.left = "0";
 
   // 2. 변수 셋팅 ///////////////////////
   // (1) 드래그 상태 변수 만들기
@@ -172,10 +184,8 @@ function goDrag(ele, coll) {
     // 마우스 다운시 주먹손!
     dtg.style.cursor = "grabbing";
 
-    // z-index 0 초기화(전체컬렉션 전달변수 coll 사용!)
-    coll.forEach(x=>x.style.zIndex = 0);
-    // z-index 1로 높이기
-    dtg.style.zIndex = 1;
+    // z-index 전역변수(zNum) 숫자를 1씩 높이기
+    dtg.style.zIndex = ++zNum;
 
     console.log("마우스 다운!", dragSts);
   }); ///////// mousedown //////////
@@ -204,11 +214,65 @@ function goDrag(ele, coll) {
     // 과도한 드래그로 갑자가 아웃되면 lastX,lastY값이
     // 셋팅되지 못한다! 이것을 기존 요소의 위치값으로 보정함!
     // 단, style위치값 코드는 'px'단위가 있으므로 parseInt처리!
-    lastX = parseInt(dtg.style.left);
-    lastY = parseInt(dtg.style.top);
+    // lastX = parseInt(dtg.style.left);
+    // lastY = parseInt(dtg.style.top);
 
     console.log("마우스나감!", dragSts);
   }); ///////// mouseleave //////////
+
+
+  //////////// 모바일 이벤트 처리 구역 //////////
+
+  // (1) 터치스타트 이벤트 함수연결하기
+  mFn.addEvt(dtg, "touchstart", (e) => {
+    // 드래그 상태값 true로 변경!
+    dTrue();
+    // 첫번째 위치포인트 셋팅!
+    firstPoint(e);
+    // 단독할당되지 않고 내부 함수호출로 연결되어있으므로
+    // 이벤트 전달을 토스해줘야 한다!(전달변수 e)
+
+    // z-index 전역변수(zNum) 숫자를 1씩 높이기
+    dtg.style.zIndex = ++zNum;
+
+    console.log("터치스타트!", dragSts);
+  }); ///////// mousedown //////////
+
+  // (2) 마우스 업 이벤트 함수연결하기
+  mFn.addEvt(dtg, "mouseup", (e) => {
+    // 드래그 상태값 false로 변경!
+    dFalse();
+    // 마지막 위치포인트 셋팅!
+    lastPoint(e);
+    
+    // 마우스 업시 편손!
+    dtg.style.cursor = "grab";
+
+    console.log("마우스 업!", dragSts);
+  }); ///////// mouseup //////////
+
+  // (3) 마우스 무브 이벤트 함수연결하기
+  mFn.addEvt(dtg, "mousemove", dMove);
+  //////////// mousemove /////////////
+
+  // (4) 마우스가 대상을 벗어나면 드래그상태값 false처리하기
+  mFn.addEvt(dtg, "mouseleave", () => {
+    // 드래그 상태값 false로 변경!
+    dFalse();
+    // 과도한 드래그로 갑자가 아웃되면 lastX,lastY값이
+    // 셋팅되지 못한다! 이것을 기존 요소의 위치값으로 보정함!
+    // 단, style위치값 코드는 'px'단위가 있으므로 parseInt처리!
+    // lastX = parseInt(dtg.style.left);
+    // lastY = parseInt(dtg.style.top);
+
+    console.log("마우스나감!", dragSts);
+  }); ///////// mouseleave //////////
+
+
+
+
+
+
 } /////////////// goDrag 함수 ///////////////
 /////////////////////////////////////////////
 
