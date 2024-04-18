a741ce8878940d242feb9d618a2475567e90fdd8
Tom B. Parker <tombap8@hotmail.com>
2024-04-18 11:15:49 +0900

    Update drag.js

--- 003.JS학습/01.JS실험실/js/drag.js
+++ 003.JS학습/01.JS실험실/js/drag.js
@@ -226,8 +226,8 @@ function goDrag(ele) {
     // 과도한 드래그로 갑자가 아웃되면 lastX,lastY값이
     // 셋팅되지 못한다! 이것을 기존 요소의 위치값으로 보정함!
     // 단, style위치값 코드는 'px'단위가 있으므로 parseInt처리!
-    // lastX = parseInt(dtg.style.left);
-    // lastY = parseInt(dtg.style.top);
+    lastX = parseInt(dtg.style.left);
+    lastY = parseInt(dtg.style.top);
 
     console.log("마우스나감!", dragSts);
   }); ///////// mouseleave //////////
