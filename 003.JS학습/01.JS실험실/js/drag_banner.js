3334cae3aaa6c0fb2edf1b275d6d5aede7ada008
Tom B. Parker <tombap8@hotmail.com>
2024-04-19 17:08:47 +0900

    sas

--- 003.JS학습/01.JS실험실/js/drag_banner.js
+++ 003.JS학습/01.JS실험실/js/drag_banner.js
@@ -1,15 +1,9 @@
 // JS실험실 : 07.드래그배너 JS - drag_banner.js
 
 // [1] 모듈함수 불러오기 파트 /////////
-// 슬라이드 기능 함수 불러오기
-import setSlide from "./slide_fn.js";
-
-// 드래그 기능 함수 호출하기
-import setDrag from "./slide_drag.js";
+// 드래그 슬라이드 기능 함수 불러오기
+import setSlide from "./drag_slide.js";
 
 // [2] 기능구현 파트 ///////////////
-// 1. 슬라이드 기능 함수 호출하기
+// 1. 드래그 슬라이드 기능 함수 호출하기
 setSlide("banbx");
-
-// 2. 드래그 기능 함수 호출하기
-setDrag("slide");
