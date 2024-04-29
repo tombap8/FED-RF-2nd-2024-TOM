c788e1c40f676a89f3a50c2406c2aa27bae12c11
Tom B. Parker <tombap8@hotmail.com>
2024-04-29 11:53:42 +0900

    Update dance.js

--- 003.JS학습/01.JS실험실/js/dance.js
+++ 003.JS학습/01.JS실험실/js/dance.js
@@ -46,6 +46,7 @@ const eachList = mFn.qsaEl(target,'li');
 mFn.addEvt(window,'scroll',moveSlide);
 
 // 3. 함수 만들기 /////////
+// (1) 슬라이드 이동함수 /////////////////
 function moveSlide(){
     // 1. 스티키 부모박스 바운딩top값
     let bTop = mFn.getBCR(tpg);
@@ -67,19 +68,37 @@ function moveSlide(){
         target.style.left = '-3000px';
     }
 
-    // 3. 스티키 박스 하위 li가 보이는화면 left 기준선
-    // 에 대한 위치값 바운딩left값을 이용하여
-    // 자기위치에서 위/아래로 이동하는 수치변경하기
-
-    let mVal = mFn.getBCR2(eachList[2]);
-    // 위치값을 양수로 만들고 윈도우 크기로 나눈후 백분율
-    // 수치가 크므로 2로 나누어주었음...
-    mVal = Math.abs(mVal/window.innerWidth)*100/2;
-    console.log('바운딩left:',mVal);
-
-    eachList[2].style.translate = `0 ${mVal}%`;
+    // 3. 개별리스트 상하 이동함수 호출하기
+    eachList.forEach((ele,idx)=>upDownFn(ele,idx));
+    // ele는 해당 리스트 li자신, idx 는 li자신의 순번
+    
     
 
 } /////////// moveSlide 함수 /////////////
 
 
+// (2) 리스트 상하 이동함수 ///////////
+function upDownFn(ele,idx){ 
+    // ele - 각 li요소 / idx  - 각 li 순번
+
+    // 스티키 박스 하위 li가 보이는화면 left 기준선
+    // 에 대한 위치값 바운딩left값을 이용하여
+    // 자기위치에서 위/아래로 이동하는 수치변경하기
+
+    // 1. 바운딩 left값 구하기
+    let mVal = mFn.getBCR2(ele);
+
+    // 2. 위치값을 양수로 만들고 윈도우 크기로 나눈후 백분율
+    // 수치가 크므로 2로 나누어주었음...
+    mVal = Math.abs(mVal/window.innerWidth)*100/2;
+    // console.log('바운딩left:',mVal);
+
+    // 3. 들어온 li순번이 짝수일 경우 마이너스 처리하기!
+    // 짝수일 경우 마이너스를 곱하여 방향을 반대로 해준다
+    // 짝수는 %연산자사용 : 숫자%2==0 이면 짝수임!
+    if(idx%2==0) mVal = -mVal;
+
+    // 4. 실제 위치이동 CSS적용하기
+    ele.style.translate = `0 ${mVal}%`;
+
+} ////////// upDownFn 함수 ////////////////
