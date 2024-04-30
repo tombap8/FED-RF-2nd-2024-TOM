998e62e21debce3b8db51bc28cdfa882c7d83adf
Tom B. Parker <tombap8@hotmail.com>
2024-04-30 12:20:04 +0900

    ss

---
+++ 004.React학습/01.react-cdn/js/car_data.jsx
@@ -0,0 +1,21 @@
+// 자동차 정보파일 /////
+const carInfo = 
+[
+    {
+        color:"라잇블루",
+        model:"2023년형",
+        opt:{filter:'hue-rotate(0deg)'}
+    },
+    {
+        color:"럭셔리와인",
+        model:"2025년형",
+        opt:{filter:'hue-rotate(109deg)',transform:'rotateY(180deg)'}
+    },
+    {
+        color:"녹차그린",
+        model:"2024년형",
+        opt:{filter:'hue-rotate(215deg)'}
+    },
+]
+
+export {carInfo};
