9fc5160776cf2586033a3d26706183034ae04e77
Tom B. Parker <tombap8@hotmail.com>
2024-05-01 16:18:28 +0900

    Update 06.Event.jsx

--- 004.React학습/01.react-cdn/js/06.Event.jsx
+++ 004.React학습/01.react-cdn/js/06.Event.jsx
@@ -14,79 +14,94 @@ import mFn from "./my_function";
 *************************************************************/
 
 /////// 전체 이벤트 적용할 컴포넌트 구성하기 //////////////////
-function EventShow(){
+function EventShow() {
+  /// 컴포넌트 리턴 코드 위에서 이벤트처리를 위한
+  // 함수를 만들어서 사용할 수 있다!!!
+  // 지역함수로 사용되는 것임!
 
-    /// 컴포넌트 리턴 코드 위에서 이벤트처리를 위한
-    // 함수를 만들어서 사용할 수 있다!!!
-    // 지역함수로 사용되는 것임!
+  // 오버시 이벤트 한번만 실행되게 상태변수만들기
+  let onceSts = false;
 
-    // 오버시 이벤트 한번만 실행되게 상태변수만들기
-    let onceSts = false;
+  // 1. 컴포넌트 내부함수 //////////
 
-    // 1. 컴포넌트 내부함수 //////////
-    // (1) 소원이 무엇이냐 물어보는 함수
-    const showAladin = () => {
-        if(onceSts) return;
-        onceSts = true; // 한번만 실행
+  // (1) 소원이 무엇이냐 물어보는 함수 ///////////
+  const showAladin = () => {
+    // 1. 한번만 실행 분기문
+    if (onceSts) return;
+    onceSts = true; // 한번만 실행
 
-        console.log('알라딘이 누구야?');
+    console.log("알라딘이 누구야?");
 
-        // html 출력대상 : #ala
-        let alaBox = mFn.qs("#ala");
+    // 2. 알라딘 이미지 출력하기
+    // (1) html 출력대상 : #ala
+    let alaBox = mFn.qs("#ala");
 
-        // 이미지출력
-        ReactDOM.render(
-        <img src="./images/ala4.jpg" alt="알라딘"/>,
-        alaBox);
+    // (2) 이미지출력
+    ReactDOM.render(<img src="./images/ala4.jpg" alt="알라딘" />, alaBox);
 
-        // 말풍선 박스에 글자넣기 ///
-        let titBox = mFn.qs(".tit");
-        titBox.innerText = "소원이 무엇이냐?";
-        // 말풍선 박스에 인라인 CSS코드 넣기
-        titBox.style.cssText = `
+    // 3. 말풍선 박스에 글자넣기 ///
+    let titBox = mFn.qs(".tit");
+    titBox.innerText = "소원이 무엇이냐?";
+
+    // 4. 말풍선 박스에 인라인 CSS코드 넣기
+    titBox.style.cssText = `
             width: 50%;
             padding: 50px 0;
             margin: 0 auto;
             border: 2px solid lime;
             opacity: 0;
         `;
-        // 0.5초후 CSS변경으로 타이틀 등장하기
-        setTimeout(() => {
-            let tg = titBox.style;
-            tg.transition = "2s ease-in-out 1s";
-            tg.opacity = 1;
-            tg.borderRadius = "50%";
-            tg.translate = "0 -200px";
-            tg.fontSize = "40px";
-            tg.color = "white";
-            tg.backgroundColor = "rgba(0,0,0,.5)";            
-        }, 500);
 
+    // 5. 0.5초후 CSS변경으로 타이틀 등장하기
+    setTimeout(() => {
+      let tg = titBox.style;
+      tg.transition = "2s ease-in-out 1s";
+      tg.opacity = 1;
+      tg.borderRadius = "50%";
+      tg.translate = "0 -200px";
+      tg.fontSize = "40px";
+      tg.color = "white";
+      tg.backgroundColor = "rgba(0,0,0,.5)";
+    }, 500);
 
+    // 6. 램프가져오기 버튼 3초후 보이기
+    setTimeout(() => {
+      mFn.qsa("button")[0].style.display = "inline-block";
+    }, 3000);
+  }; ////////// showAladin 함수 ///////////
 
-    }; ////////// showAladin 함수 ///////////
+  // (2) 램프가져오기 함수 ////////////
+  const getLamp = () => {
+    console.log("램프 가져와~!!!");
+  }; ////////// getLamp 함수 ///////////
 
-    /// 2. 리턴 코드 만들기 ////////////
-    return(
-        <React.Fragment>
-            <div id="tbox" style={{textAlign:"center"}}>
-                {/* 스타일 인라인 적용시 바깥중괄호는 표현식
+  /// 2. 리턴 코드 만들기 ////////////
+  return (
+    <React.Fragment>
+      <div id="tbox" style={{ textAlign: "center" }}>
+        {/* 스타일 인라인 적용시 바깥중괄호는 표현식
                 내부 중괄호는 객체형식의 스타일 설정임! */}
-                <img 
-                src="./images/genie.avif" alt="지니" 
-                /* 마우스오버시 showAladin함수호출 */
-                onMouseOver={showAladin}
-                />
-                {/* 소원이 무엇이냐 말풍선박스 */}
-                <div className="tit"></div>
+        <img
+          src="./images/genie.avif"
+          alt="지니"
+          /* 마우스오버시 showAladin함수호출 */
+          onMouseOver={showAladin}
+        />
 
-            </div>
+        {/* 램프가 들어갈 요소 */}
+        <div className="lamp"></div>
 
-        </React.Fragment>
-    );
+        {/* 버튼들 */}
+        <button onClick={getLamp}>램프가져오기~!</button>
+        <button>소원빌기~! 페라리주세요~!!!</button>
 
+        {/* 소원이 무엇이냐 말풍선박스 */}
+        <div className="tit"></div>
+      </div>
+    </React.Fragment>
+  );
 } /////////// EventShow 컴포넌트 /////////////////
 
 // 화면출력하기 ////////////
 // ReactDOM.render(넣을코드,대상)
-ReactDOM.render(<EventShow />,mFn.qs("#root"));
+ReactDOM.render(<EventShow />, mFn.qs("#root"));
