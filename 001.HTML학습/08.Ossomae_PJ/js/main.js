8536e5ca9e0767090e4189f821bbf11a89801087
Tom B. Parker <tombap8@hotmail.com>
2024-05-03 13:09:11 +0900

    ss

--- 001.HTML학습/08.Ossomae_PJ/js/main.js
+++ 001.HTML학습/08.Ossomae_PJ/js/main.js
@@ -75,3 +75,34 @@ import mFn from "./my_function.js";
         } //// else ////
 
     } ////////changeSlide 함수 //////////////
+
+
+    // 자동넘김용 호출함수 ///////////////
+    // 오른쪽 이동함수 ///
+    const goRight = () => 
+    gbx.appendChild(mFn.qsaEl(gbx,"div")[0]);
+
+    // 자동넘김용 변수
+    // (인터발용: autoI, 타임아웃용:autoT)
+    let autoI, autoT;
+
+    // 인터발 호출함수 ////
+    const autoSlide = () => 
+    autoI = setInterval(goRight,3000);
+
+    // 인터발 함수 최초호출 ////
+    autoSlide();
+
+    // 인터발지우기 함수 ////
+    const clearAuto = () => {
+        // 인터발지우기
+        clearInterval(autoI);
+        // 타임아웃지우기(실행쓰나미방지)
+        clearTimeout(autoT);
+        // 일정시간후 작동
+        autoT = setTimeout(autoSlide, 5000);
+    }; ////// clearAuto 함수 //////
+
+    // 버튼 클릭시 clearAuto함수 호출하기 ////
+    abtn.forEach(
+        ele=>mFn.addEvt(ele,"click",clearAuto));
--- 001.HTML학습/08.Ossomae_PJ/js/main_jquery.js
+++ 001.HTML학습/08.Ossomae_PJ/js/main_jquery.js
@@ -21,11 +21,34 @@
     let stopClick = false;
     // 애니시간(잠금시간)
     const TIME_SLIDE = 400;
+    // 변경대상
+    let gbx = $(".gbx");
 
     // 3. 이벤트 설정 및 기능구현하기 //////////
     $(".abtn").click(function(){
+        // 0. 광클금지 //////
+        if(stopClick) return;
+        stopClick = true;
+        setTimeout(()=>stopClick=false, TIME_SLIDE);
+
         // 버튼자신은 this키워드 사용
         console.log("나야나!",$(this).is(".rb"));
+        // is() 메서드는 선택요소의 클래스 등 확인기능
+        
+        // find() 는 하위요소를 모두 선택한다!
+        // 참고) 직계요소만 선택할때는 children()사용!
+        // first() 는 첫번째, last() 는 마지막째
+        // eq(n) 는 n번째 요소를 선택함!
+
+        // 1. 오른쪽버튼 분기 : 맨앞div 맨뒤이동
+        if($(this).is(".rb")){
+            gbx.append(gbx.find("div").first());
+        } /// if ///
+
+        // 2. 왼쪽버튼 분기 : 맨뒤div 맨앞이동
+        else{
+            gbx.prepend(gbx.find("div").last());
+        } /// else ///
     }); //////// click ////////////
     
 
