803afcb44b9d40433dda06691c132ab32188848d
Tom B. Parker <tombap8@hotmail.com>
2024-05-13 16:08:26 +0900

    ss

--- 001.HTML학습/05.Nav_PJ/js/gnb_menu.js
+++ 001.HTML학습/05.Nav_PJ/js/gnb_menu.js
@@ -10,24 +10,45 @@ export default function makeMenu(target){
     target.innerHTML = `
     <ul>
     ${
+        // mdata는 객체이므로 객체키만 모아서 배열로 만들고
+        // map() 메서드를 순회하여 코드를 반복 생성함!
+        // Object.keys(객체).map().join("")
+        // -> 오브젝트 키쓰 맵 쪼잉~!!!
         Object.keys(mdata).map(v=>`        
         <li>
             <a href="#">${v}</a>
             <div class="smenu">
-            <aside class="smbx">
-                <h2>
-                <div class="stit">1차메뉴</div>
-                <a href="#">전체보기</a>
-                </h2>
-                <div class="swrap">
-                <!-- 2차메뉴 dl생성 -->
-                <dl>
-                    <dt>2차메뉴</dt>
-                    <!-- 3차메뉴 dd생성 -->
-                    <dd><a href="#">3차메뉴</a></dd>
-                </dl>
-                </div>
-            </aside>
+                <aside class="smbx">
+                    <h2>
+                    <div class="stit">${v}</div>
+                        <a href="#">전체보기</a>
+                    </h2>
+                    <div class="swrap">
+                    <!-- 2차메뉴 dl생성 -->
+                    ${
+                        // mdata[키배열값] -> 키배열값은 v
+                        // 그런데 이 데이터는 객체이므로
+                        // map()을 쓰기위해 또 키배열로 뽑아낸다!
+                        // v2키값은 2차메뉴임!
+                        Object.keys(mdata[v])
+                        .map(v2=>`
+                            <dl>
+                                <dt>${v2}</dt>
+                                <!-- 3차메뉴 dd생성 -->
+                                ${
+                                    // mdata[v][v2] -> 3차메뉴배열
+                                    mdata[v][v2]
+                                    .map(v3=>`                                    
+                                        <dd>
+                                            <a href="#">${v3}</a>
+                                        </dd>
+                                    `).join('')
+                                }
+                            </dl>                        
+                        `).join('')
+                    }
+                    </div>
+                </aside>
             </div>
         </li>
         `).join('')
