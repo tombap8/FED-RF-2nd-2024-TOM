d9b93a632ad3adbf875c54537fe2ab2871a01a8a
Tom B. Parker <tombap8@hotmail.com>
2024-05-14 13:11:40 +0900

    ss

--- 004.React학습/02.react-ex/js/01.공유신발.jsx
+++ 004.React학습/02.react-ex/js/01.공유신발.jsx
@@ -1,5 +1,10 @@
 // 01.공유신발 JSX
 
+// 공유신발 데이터 불러오기
+import guData from "./data/gu_data";
+
+// console.log(guData);
+
 // [ 메인 컴포넌트 ] ////////
 // 메인의 의미는? 다른 구성요소 컴포넌트들을 모아
 // 최종ㅈ거으로 랜더링하는 구성 컴포넌트를 말한다!
@@ -8,10 +13,8 @@
 // 특정 모듈로 구성된 HTML코드를 리턴하는 객체
 
 // 함수형 컴포넌트는 첫글자 대문자인 함수키워드로 만든다
-function MainComponent(){
-
-
-    /************************************** 
+function MainComponent() {
+  /************************************** 
         [ 코드구성 ]
         1. 타이틀 : h1.tit
         2. 내용박스 : section
@@ -25,38 +28,45 @@ function MainComponent(){
         ㄴ> 상품상세보기 :
             ol > li > (img/text/button)
     **************************************/
-    // 코드리턴구역 ////////////////
-    return(
-        <React.Fragment>
-            {/* 1. 타이틀 */}
-            <h1 className="tit">
-                공유가 신고 다닌다는!
-            </h1>
-            {/* 2. 내용박스 */}
-            <section>
-                <h2>공유는 오늘도 멋찝니다!</h2>
-                <div className="img-box">
-                    <img 
-                    src="./images/vans/gongyoo.jpg" 
-                    alt="멋진공유" />
-                </div>
-            </section>
-            {/* 3. 기능버튼박스 */}
-            <div className="btn-box">
-                <button>효진초이스 바로가기</button>
-            </div>
-            {/* 4. 상품리스트박스 */}
-            <div className="gwrap">
-
-            </div>
-            
-
-        </React.Fragment>
-    );
+  // 코드리턴구역 ////////////////
+  return (
+    <React.Fragment>
+      {/* 1. 타이틀 */}
+      <h1 className="tit">공유가 신고 다닌다는!</h1>
+      {/* 2. 내용박스 */}
+      <section>
+        <h2>공유는 오늘도 멋찝니다!</h2>
+        <div className="img-box">
+          <img src="./images/vans/gongyoo.jpg" alt="멋진공유" />
+        </div>
+      </section>
+      {/* 3. 기능버튼박스 */}
+      <div className="btn-box">
+        <button>효진초이스 바로가기</button>
+      </div>
+      {/* 4. 상품리스트박스 */}
+      <div className="gwrap">
+        <ul>
+          {guData.map((v) => (
+            <li>
+              <a href="#">
+                <ol className="glist">
+                  <li>
+                    <img src={`./images/vans/vans_${v.idx}.jpg`} alt="신발" />
+                  </li>
+                  <li>{v.gname}</li>
+                  <li>가격 : {v.gprice}원</li>
+                </ol>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </React.Fragment>
+  );
 } ////////// MainComponent 컴포넌트 /////////////
 
 // 메인 컴포넌트 출력하기 ////////////
-ReactDOM.render(<MainComponent />,
-document.querySelector("#root"));
+ReactDOM.render(<MainComponent />, document.querySelector("#root"));
 // ReactDOM.render(어쩌구,저쩌구);
 // 어쩌구를 저쩌구에 출력해라!
