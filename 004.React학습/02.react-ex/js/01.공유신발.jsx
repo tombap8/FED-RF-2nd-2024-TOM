09bf6dfa3cb1095af3e029b8f05efd14242864c7
Tom B. Parker <tombap8@hotmail.com>
2024-05-14 12:06:50 +0900

    sss

---
+++ 004.React학습/02.react-ex/js/01.공유신발.jsx
@@ -0,0 +1,47 @@
+// 01.공유신발 JSX
+
+// [ 메인 컴포넌트 ] ////////
+// 메인의 의미는? 다른 구성요소 컴포넌트들을 모아
+// 최종ㅈ거으로 랜더링하는 구성 컴포넌트를 말한다!
+
+// 그렇다면 컴포넌트란?
+// 특정 모듈로 구성된 HTML코드를 리턴하는 객체
+
+// 함수형 컴포넌트는 첫글자 대문자인 함수키워드로 만든다
+function MainComponent(){
+
+
+    /************************************** 
+        [ 코드구성 ]
+        1. 타이틀 : h1.tit
+        2. 내용박스 : section
+        ㄴ> 제목 : h2
+        ㄴ> 이미지박스 : div.img-box > img
+        3. 기능버튼박스 : div.btn-box
+        ㄴ> 기능버튼 : button
+        4. 상품리스트박스 : div.gwrap
+        ㄴ> 상품리스트 : 
+            a > ol > li > (img/text)
+        ㄴ> 상품상세보기 :
+            ol > li > (img/text/button)
+    **************************************/
+    // 코드리턴구역 ////////////////
+    return(
+        <React.Fragment>
+            {/* 1. 타이틀 */}
+            <h1 className="tit">
+                공유가 신고 다닌다는!
+            </h1>
+            {/* 2. 내용박스 */}
+            <section>
+                <h2>공유는 오늘도 멋찝니다!</h2>
+                <div className="img-box">
+                    <img 
+                    src="./images/vans/gongyoo.jpg" 
+                    alt="멋진공유" />
+                </div>
+            </section>
+
+        </React.Fragment>
+    );
+} ////////// MainComponent 컴포넌트 /////////////
