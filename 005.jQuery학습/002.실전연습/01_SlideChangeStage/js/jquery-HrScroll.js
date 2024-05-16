a61b28bacc102f1ed6020b962db742980d5b95dd
Tom B. Parker <tombap8@hotmail.com>
2024-05-16 11:58:08 +0900

    Update jquery-HrScroll.js

--- 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-HrScroll.js
+++ 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-HrScroll.js
@@ -8,6 +8,9 @@ const scTarget = $("html,body");
 // 대상은 항상 html,body 로 두개 모두 잡아준다!
 // 참고로 document나 window는 사용안됨!
 // 이벤트 메서드: on(이벤트명,함수)
+// 유의사항: 휠 이벤트는 모바일과 무관하다!
+// 따라서 모바일 터치 가로 스크롤은 그냥
+// 가로 스크롤바만 살려주면 자연스럽게 된다!
 
 // 스크롤 위치값변수
 let scPos = 0;
@@ -42,15 +45,30 @@ chgLimit();
 $(window).resize(chgLimit);
 // resize() 메서드 :  리사이즈 이벤트함수
 
+/////////////////////////////////////
+// 스크롤 이벤트 함수 구현하기 /////////
+/////////////////////////////////////
 scTarget.on("wheel", (e) => {
   // 스크롤이동을 위한 제이쿼리 속성
   // 1. scrollTop : 세로스크롤바위치
   // 2. scrollLeft : 가로스크롤바위치
 
   // 휠방향 알아내기(전체이벤트객체로 부터 얻어옴)
-  let delta = event.wheelDelta;
+  // (1) evnet.wheelDelta 값 :
+  // 기본이동 100px + 앞뒤예비공백 10px*2=20px
+  // 전체 표시수치 120 -> 120px을 의미
+  // 방향은 마이너스가 아랫방향
+  //   let delta = event.wheelDelta;
+  // (2) event.deltaY
+  // 기본이동크기만 표시함 즉, 100px이동값표시
+  // 방향은 양수가 아랫방향
+  // -> wheelDelta보다 나중에 나온 실질적인 표시객체임
+  //
+  let delta = event.deltaY;
 
-  if (delta < 0) scPos += 200;
+  // 방향에 따를 증감은 deltaY는 양수가 아랫방향
+  // wheelDelta는 음수가 아랫방향임!
+  if (delta > 0) scPos += 200;
   else scPos -= 200;
 
   // 한계값 체크
