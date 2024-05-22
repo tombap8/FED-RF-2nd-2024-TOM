571124df7a47558188f645f9041707256844a905
Tom B. Parker <tombap8@hotmail.com>
2024-05-22 10:53:21 +0900

    ss

--- 003.JS학습/js/my_function.js
+++ 003.JS학습/js/my_function.js
@@ -1,9 +1,9 @@
 // 공통함수 JS - my_function.js
 
 // 객체를 내보내기 : 변수선언과 이름없이 바로 직접 내보냄!
-// const myFn = 
 // -> 선언과 할당후 export default하려면 하단에서 해야함!
-export default {
+// export default {
+const mFn = {
     // 요소선택함수 ////////
     qs: (x) => document.querySelector(x),
     qsEl: (el, x) => el.querySelector(x),
@@ -19,3 +19,6 @@ export default {
     // 옵셋탑값 반환함수
     getOT: (ele) => ele.offsetTop,
   };
+
+  // 내보내기
+  export default mFn;
