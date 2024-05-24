2fe16abc8c93785dc406e542b6ca12be9fff8202
Tom B. Parker <tombap8@hotmail.com>
2024-05-24 17:14:22 +0900

    Update ItemsArea.jsx

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
@@ -3,26 +3,35 @@
 // 아이템 카테고리 데이터 불러오기
 import catData from "../data/category";
 
-export default function ItemsArea({catName}) {
+export default function ItemsArea({ catName }) {
   // catName - 카테고리 분류 이름(데이터 객체명과 동일!)
 
   // 해당 카테고리의 데이터 선택하여 담기
   const selData = catData[catName];
   console.log(selData);
 
-
   // 코드 리턴구역 /////
   return (
     <div id="main-area">
       {/* 데이터적용1 : 최상위 클래스명 추가하기 */}
-      <main className={"main-area ibx "+selData.경로}>
+      <main className={"main-area ibx " + selData.경로}>
         {/* <!-- 2-1. 카테고리 페이지 상단영역 --> */}
         <header className="cat-top-area">
           {/* <!-- 2-1-1. 서브타이틀 --> */}
           {/* 데이터적용2 : 제목넣기 */}
           <h2 className="cat-tit">{selData.제목}</h2>
           {/* <!-- 2-1-2. 서브메뉴(LNB:Local Navigation Bar) --> */}
-          <nav className="lnb"></nav>
+          <nav className="lnb">
+            {
+              <ul>
+                {selData.메뉴.map((v) => (
+                  <li>
+                    <a href="#">{v}</a>
+                  </li>
+                ))}                
+              </ul>
+            }
+          </nav>
         </header>
         {/* <!-- 2-2. 카테고리 페이지 컨텐츠영역 --> */}
         <div className="cat-cont-area">
