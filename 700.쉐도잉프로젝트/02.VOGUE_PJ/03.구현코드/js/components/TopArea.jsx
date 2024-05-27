5d86eeba75985deeba840f749a896ad0e40d5bc4
Tom B. Parker <tombap8@hotmail.com>
2024-05-27 13:22:15 +0900

    ss

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/TopArea.jsx
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/TopArea.jsx
@@ -1,6 +1,33 @@
 // 상단영역 컴포넌트 ///////
 
-export default function TopArea() {
+export default function TopArea({ changeMenu }) {
+  // changeMenu - 부모의 setMenu 상태관리 메서드
+
+  // 메뉴 배열
+  const menuArr = [
+    "FASHION",
+    "BEAUTY",
+    "LIVING",
+    "PEOPLE",
+    "VIDEO",
+    "RUNWAY",
+    "TIME & GEM",
+    "SHOPPING",
+  ];
+
+  // 메뉴 변경하기 함수 ///////
+  const chgMenuFn = (v) => {
+
+    console.log(v);
+
+    // 전달값 변경하기 : 소문자,특수문자 변경
+    v = v.toLowerCase().replace(" & ", "-");
+
+    // 부모 메뉴변경 상태메서드로 메뉴변경
+    changeMenu(v);
+
+  }; ////////// chgMenuFn 함수 /////////
+
   // 코드 리턴구역 /////
   return (
     <div id="top-area">
@@ -61,37 +88,24 @@ export default function TopArea() {
         </div>
         {/* <!-- 1-2.로고박스 --> */}
         <h1 className="logo">
-          <a href="#">
+          <a href="#" onClick={()=>chgMenuFn("home")}>
             <img src="./images/mlogo.png" alt="메인로고" />
           </a>
         </h1>
         {/* <!-- 1-3.GNB박스 --> */}
         <nav className="gnb">
           <ul>
-            <li>
-              <a href="#">FASHION</a>
-            </li>
-            <li>
-              <a href="#">BEAUTY</a>
-            </li>
-            <li>
-              <a href="#">LIVING</a>
-            </li>
-            <li>
-              <a href="#">PEOPLE</a>
-            </li>
-            <li>
-              <a href="#">VIDEO</a>
-            </li>
-            <li>
-              <a href="#">RUNWAY</a>
-            </li>
-            <li>
-              <a href="#">TIME &amp; GEM</a>
-            </li>
-            <li>
-              <a href="#">SHOPPING</a>
-            </li>
+            {menuArr.map((v) => (
+              <li>
+                <a
+                  href="#"
+                  onClick={() => {chgMenuFn(v)}}
+                >
+                  {v}
+                </a>
+              </li>
+            ))}
+
             <li>
               {/* <!-- 돋보기 검색버튼 --> */}
               <i href="#" className="fi fi-search">
--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
@@ -4,18 +4,26 @@
 import TopArea from "./components/TopArea";
 // 메인영역 불러오기
 import MainArea from "./components/MainArea";
+// 아이템영역 불러오기
+import ItemsArea from "./components/ItemsArea";
 // 하단영역 불러오기
 import FooterArea from "./components/FooterArea";
 
 // [1] 메인 페이지 전체 레이아웃 로딩 컴포넌트 ///
 function Layout() {
-  // 코드 리턴구역
+  // 상태관리변수 설정구역 ////////
+  // [1] 메뉴 변경 상태변수
+  const [menu, setMenu] = React.useState("people");
+
+
+
+  // 코드 리턴구역 ////////////
   return (
     <React.Fragment>
       {/* 1. 상단영역 컴포넌트 */}
-      <TopArea />
+      <TopArea changeMenu={setMenu} />
       {/* 2. 메인영역 컴포넌트 */}
-      <MainArea />
+      {menu=="home"?<MainArea />:<ItemsArea catName={menu} />}
       {/* 3. 하단영역 컴포넌트 */}
       <FooterArea />
     </React.Fragment>
