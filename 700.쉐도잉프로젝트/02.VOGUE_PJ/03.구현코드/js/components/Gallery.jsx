34207ae5ad780f5dfc1cbe65891875c26d848392
Tom B. Parker <tombap8@hotmail.com>
2024-05-27 16:12:37 +0900

    ss

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/Gallery.jsx
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/Gallery.jsx
@@ -1,20 +1,90 @@
 // 보그 JS 갤러리 컴포넌트 - Gallery.jsx
 
 export default function Gallery() {
+
+
+    // 스와이퍼 인스턴스 생성함수 ////
+    const setSwiper = ()=>{
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+            breakpoints: {
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+              },
+            },
+          });
+
+    };
+
+  // 화면 랜더링전 실행구역 /////
+  React.useLayoutEffect(() => {
+    // 스와이퍼 생성 함수 호출
+    setSwiper();
+  }); ///// useLayoutEffect //////////
+
   // 코드 리턴구역 /////
   return (
-    <div id="main-area">
-      <main className="main-area ibx">
-        {/* <!-- 2-1. 로그인 페이지 상단영역 --> */}
-        <header className="ctop">
-          {/* <!-- 2-1-1. 서브타이틀 --> */}
-          <h2 className="stit">Gallery</h2>
-        </header>
-        {/* <!-- 2-2. 갤러리 페이지 컨텐츠 박스 --> */}
-        <section className="scont">
+    <React.Fragment>
+      
+      <div id="main-area">
+        <main className="main-area ibx">
+          {/* <!-- 2-1. 로그인 페이지 상단영역 --> */}
+          <header className="ctop">
+            {/* <!-- 2-1-1. 서브타이틀 --> */}
+            <h2 className="stit">Gallery</h2>
+          </header>
+          {/* <!-- 2-2. 갤러리 페이지 컨텐츠 박스 --> */}
+          <section className="scont">
             {/* <!-- Swiper --> */}
-        </section>
-      </main>
-    </div>
+            <div className="swiper mySwiper">
+              <div className="swiper-wrapper">
+                <div className="swiper-slide">
+                  <img src="images/people/cont2-1a.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/fashion/cont1-2b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/people/cont2-3a.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/fashion/cont2-1b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/fashion/cont2-2b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/fashion/cont2-3b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/beauty/cont1-2a.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/beauty/cont2-3b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/beauty/cont1-1b.jpg" alt="보그갤러리" />
+                </div>
+              </div>
+              <div className="swiper-pagination"></div>
+            </div>
+          </section>
+        </main>
+      </div>
+    </React.Fragment>
   );
 } /////////// Gallery 컴포넌트 //////////////
