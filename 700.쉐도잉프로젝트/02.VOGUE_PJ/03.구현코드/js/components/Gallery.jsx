90c0bc854911a5fc1965db57efd47cb94a59afdb
Tom B. Parker <tombap8@hotmail.com>
2024-05-27 16:53:54 +0900

    ss

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/Gallery.jsx
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/Gallery.jsx
@@ -1,39 +1,53 @@
 // 보그 JS 갤러리 컴포넌트 - Gallery.jsx
 
 export default function Gallery() {
+  // 스와이퍼 인스턴스 생성함수 ////
+  const setSwiper = () => {
+    var swiper = new Swiper(".mySwiper", {
+      // 한 화면당 슬라이드수(아래breakpoint로 설정함)
+      slidesPerView: 1,
+      // 슬라이드 사이간격
+      spaceBetween: 10,
+      // 무한넘기기
+      loop: true,
+      // 자동넘기기
+      autoplay: {
+        // 사이간격
+        delay: 2500,
+        // 상호작용 죽이기 속성 없앰!
+        disableOnInteraction: false,
+      },
+      // 하단 블릿표시
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      // 양쪽이동버튼
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
 
-
-    // 스와이퍼 인스턴스 생성함수 ////
-    const setSwiper = ()=>{
-        var swiper = new Swiper(".mySwiper", {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            pagination: {
-              el: ".swiper-pagination",
-              clickable: true,
-            },
-
-            /* 가로 사이즈별 스와이퍼 설정변경 */
-            breakpoints: {                
-              // 가로 200px이상
-              200: {
-                slidesPerView: 1,
-                spaceBetween: 0,
-              },
-              // 가로 700px이상
-              700: {
-                slidesPerView: 2,
-                spaceBetween: 10,
-              },
-              // 가로 1000px이상
-              1000: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-              },
-            },
-          });
-
-    };
+      /* 가로 사이즈별 스와이퍼 설정변경 */
+      breakpoints: {
+        // 가로 200px이상
+        200: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        // 가로 700px이상
+        700: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        // 가로 1000px이상
+        1000: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+      },
+    });
+  };
 
   // 화면 랜더링전 실행구역 /////
   React.useLayoutEffect(() => {
@@ -83,6 +97,9 @@ export default function Gallery() {
                 <img src="images/beauty/cont1-1b.jpg" alt="보그갤러리" />
               </div>
             </div>
+
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
             <div className="swiper-pagination"></div>
           </div>
         </section>
