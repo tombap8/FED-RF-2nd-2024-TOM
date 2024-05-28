23a44de3425206a621de4b82b665d62e610a5997
Tom B. Parker <tombap8@hotmail.com>
2024-05-28 11:55:52 +0900

    ss

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/Gallery.jsx
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/Gallery.jsx
@@ -1,9 +1,12 @@
 // 보그 JS 갤러리 컴포넌트 - Gallery.jsx
 
 export default function Gallery() {
+
+  // 스와이퍼 인스턴스 변수
+  let swiper;
   // 스와이퍼 인스턴스 생성함수 ////
   const setSwiper = () => {
-    var swiper = new Swiper(".mySwiper", {
+    swiper = new Swiper(".mySwiper", {
       // 한 화면당 슬라이드수(아래breakpoint로 설정함)
       slidesPerView: 1,
       // 슬라이드 사이간격
@@ -52,7 +55,21 @@ export default function Gallery() {
   // 화면 랜더링전 실행구역 /////
   React.useLayoutEffect(() => {
     // 스와이퍼 생성 함수 호출
-    setSwiper();
+    setSwiper();  
+
+    // 스와이퍼 API 테스트
+    // 갤러리 제목(.stit)클릭시 다음슬라이드 넘기기
+    $(".stit").click(()=>{
+      console.log("ㅎㅎㅎ");
+      // 변수에 담은 스와이퍼 인스턴스의
+      // 구체적인 API 메서드를 호출하여
+      // 기능을 수행한다!
+      // swiper.slideNext();
+      swiper.slidePrev();
+      // 스와이퍼 API는 여기서 참조
+      // https://swiperjs.com/swiper-api
+    });
+    
   }); ///// useLayoutEffect //////////
 
   // 코드 리턴구역 /////
