47a13bc50e3644c6767b9f6eb3156eb1e920bcda
Tom B. Parker <tombap8@hotmail.com>
2024-05-29 17:09:45 +0900

    ss

--- 004.React학습/04.react-app/02.리액트소스/tom-app/src/App.js
+++ 004.React학습/04.react-app/02.리액트소스/tom-app/src/App.js
@@ -37,7 +37,7 @@ function App() {
           />
         </span>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          이제 리액트는 내겁니다~!
         </p>
         <a
           className="App-link"
