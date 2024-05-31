8cc7a7fa4fee25f434e8cd75617c37e09fd4c33e
Tom B. Parker <tombap8@hotmail.com>
2024-05-31 16:22:51 +0900

    ss

--- 004.React학습/04.react-app/02.리액트소스/tom-app/src/index.js
+++ 004.React학습/04.react-app/02.리액트소스/tom-app/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 사스(Sass)팩키지를 설치했다면 바로 사스사용가능
+// 확장자를 여기서는 쓰고 사스파일 import에선 생략가능
+import "./css/main.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root'));
 root.render(
   <>
-   <App />
+   <h1><b>선재업고</b><span>튀어</span></h1>
   </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
