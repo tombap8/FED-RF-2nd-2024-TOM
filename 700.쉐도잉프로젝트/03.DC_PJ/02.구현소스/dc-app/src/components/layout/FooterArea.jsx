ace7d935b26b4144aa7900edf5c3808ad823bcd3
Tom B. Parker <tombap8@hotmail.com>
2024-06-04 16:17:07 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/FooterArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/FooterArea.jsx
@@ -2,6 +2,12 @@
 
 import Logo from "../modules/Logo";
 
+// 하단메뉴 데이터 불러오기
+import { bmData } from "../data/bmenu";
+
+// 하단영역 CSS 불러오기
+import "../../css/footer_area.scss";
+
 export default function FooterArea() {
   //// 코드 리턴구역 //////////////
   return (
@@ -13,7 +19,19 @@ export default function FooterArea() {
         </li>
         <li>
           {/* 하단링크박스 */}
-          <ol className="bmenu"></ol>
+          <ol className="bmenu">
+            {bmData.map((v, i) => (
+              <li key={i}>
+                <a href={v.link} target="_blank">
+                  {
+                    v.txt.toUpperCase()
+                    // toUpperCase() 대문자변환
+                    // toLowerCase() 소문자변환
+                  }
+                </a>
+              </li>
+            ))}
+          </ol>
         </li>
         <li>© & ™ DC. ALL RIGHTS RESERVED</li>
       </ul>
