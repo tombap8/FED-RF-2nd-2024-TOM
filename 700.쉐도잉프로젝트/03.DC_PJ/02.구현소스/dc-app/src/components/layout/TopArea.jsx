8830a415b9266a714e0223e461a580ee6a422749
Tom B. Parker <tombap8@hotmail.com>
2024-06-04 15:47:37 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -2,10 +2,11 @@
 
 // GNB 데이터 불러오기
 import { Link } from "react-router-dom";
-import {menu} from "../data/gnb";
+import { menu } from "../data/gnb";
 
 // 상단영역 CSS 불러오기
 import "../../css/top_area.scss";
+import Logo from "../modules/Logo";
 
 export default function TopArea() {
   //// 코드 리턴구역 //////////////
@@ -14,19 +15,20 @@ export default function TopArea() {
       {/* 1.상단영역 */}
       <header className="top-area">
         {/* 로그인 환영메시지 박스 */}
-        
+
         {/* 네비게이션 GNB파트 */}
         <nav className="gnb">
           <ul>
             {/* 1. 로고 컴포넌트 */}
             <li>
-              
+              <Logo />
             </li>
             {/* 2. GNB메뉴 데이터 배열로 만들기 */}
-            {menu.map((v,i)=>
-            <li key={i}>
+            {menu.map((v, i) => (
+              <li key={i}>
                 <Link to={v.link}>{v.txt}</Link>
-            </li>)}
+              </li>
+            ))}
           </ul>
         </nav>
       </header>
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Logo.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Logo.jsx
@@ -4,11 +4,39 @@ import React from "react";
 // 이미지 경로 데이터 불러오기
 import { isrc } from "../data/img_src";
 
-export default function Logo(){
+export default function Logo({logoStyle}){
+    // logoStyle : 상단, 하단 로고 구분코드
+    // 코드 값 : top(상단), bottom(하단)
+    console.log(isrc.logo, logoStyle);
+
+    // 객체형 스타일 적용
+    const myStyle = {
+        top:{
+            width:"45px",
+            height:"45px",
+            marginRight:"30px",
+            borderRadius:"50%",
+            cursor:"pointer"
+        },
+
+        bottom:{
+            height: "80px"
+        }
+    };
+
+    // 로고 이미지 스타일 객체
+    const imgStyle = {
+        top: "45px",
+        bottom: "80px",
+    };
+
+
     // 코드 리턴 구역 ////////
     return (        
-        <h1>
-            <img src={isrc.logo} alt="DC Logo" />
+        <h1 style={myStyle[logoStyle]}>
+            <img
+            style={imgStyle}
+            src={isrc.logo} alt="DC Logo" />
         </h1>
     );
 } ///////////// Logo ///////////////
