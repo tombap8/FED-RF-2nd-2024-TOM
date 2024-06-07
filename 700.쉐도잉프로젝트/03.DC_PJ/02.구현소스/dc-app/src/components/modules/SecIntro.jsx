1cd1a6c748cc2e055d62ce0c75082349f248cb90
Tom B. Parker <tombap8@hotmail.com>
2024-06-07 16:08:25 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SecIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SecIntro.jsx
@@ -1,18 +1,44 @@
 // DC.com 섹션소개모듈 -  SecIntro.jsx
-import React from 'react';
+import React from "react";
 
 // 섹션소개모듈 데이터 가져오기
-import { secIntroData } from '../data/sec_intro';
+import { secIntroData } from "../data/sec_intro";
 
 // 섹션소개모듈 CSS 불러오기
-import "../../css/";
+import "../../css/sec_intro.scss";
+import { Link } from "react-router-dom";
 
 function SecIntro(props) {
-    return (
-        <div>
-            섹션소개모듈
-        </div>
-    );
+  // 불러온 데이터 변수할당
+  const selData = secIntroData;
+
+  return (
+    <>
+      <section className="sec-intro">
+        {/* 반복단위박스 */}
+        {selData.map((v, i) => (
+          <div key={i}>
+            {/* 1. 이미지박스 */}
+            <div className="imbx">
+              <img src={v.isrc} alt={v.tit.split("^")[0]} />
+              {/* split()으로 자르면 배열이된다! */}
+            </div>
+            {/* 2. 타이틀박스 */}
+            <div className="titbx">
+              <h3>{v.tit.split("^")[0]}</h3>
+              <h2>{v.tit.split("^")[1]}</h2>
+            </div>
+            {/* 3. 버튼박스 */}
+            <div className="btnbx">
+              <Link to={v.link}>
+                <button>{v.btn}</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </section>
+    </>
+  );
 }
 
 export default SecIntro;
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -1,6 +1,7 @@
 // 메인 페이지 컴포넌트 ///
 
 import Banner from "../modules/Banner";
+import SecIntro from "../modules/SecIntro";
 
 export default function Main(){
 
@@ -10,6 +11,9 @@ export default function Main(){
            {/* 1. 배너 컴포넌트
            "main"이름 뒤의 숫자는 1~3사이 랜덤수 */}
            <Banner catName={"main"+Math.ceil(Math.random()*3)} />
+
+           {/* 2. 섹션소개 컴포넌트 */}
+           <SecIntro />
 
         </>
     );
