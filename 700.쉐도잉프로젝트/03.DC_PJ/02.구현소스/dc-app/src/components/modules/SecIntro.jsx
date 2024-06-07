7884edb1dd9406f0cfd8a480cd4d14c1e88f94ef
Tom B. Parker <tombap8@hotmail.com>
2024-06-07 15:21:57 +0900

    ss

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SecIntro.jsx
@@ -0,0 +1,18 @@
+// DC.com 섹션소개모듈 -  SecIntro.jsx
+import React from 'react';
+
+// 섹션소개모듈 데이터 가져오기
+import { secIntroData } from '../data/sec_intro';
+
+// 섹션소개모듈 CSS 불러오기
+import "../../css/";
+
+function SecIntro(props) {
+    return (
+        <div>
+            섹션소개모듈
+        </div>
+    );
+}
+
+export default SecIntro;
