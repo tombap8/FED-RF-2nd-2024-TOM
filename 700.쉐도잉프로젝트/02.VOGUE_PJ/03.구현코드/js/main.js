4593f31924ea2c29e55653b0260a069c54ad529f
Tom B. Parker <tombap8@hotmail.com>
2024-06-10 11:17:54 +0900

    ss

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
@@ -14,6 +14,8 @@ import Gallery from "./components/Gallery";
 import Login from "./components/Login";
 // 회원가입 모듈 불러오기
 import Member from "./components/Member";
+// 부드러운 스크롤 불러오기
+import { scrolled, setPos } from "./smoothScroll24";
 
 // [1] 메인 페이지 전체 레이아웃 로딩 컴포넌트 ///
 function Layout() {
@@ -22,19 +24,19 @@ function Layout() {
   const [menu, setMenu] = React.useState("home");
 
   // 화면 랜더링 직전에 CSS로딩 변경하기 /////
-  React.useLayoutEffect(()=>{
+  React.useLayoutEffect(() => {
     // menu 상태변수에 의존시킨다!
     // 메인 css 대상요소 : #main-css
-    document.querySelector("#main-css").href=
-    menu=="home" 
-    ? "./css/main.css" 
-    : menu=="gallery" 
-    ? "./css/gallery.css" 
-    : menu=="login" 
-    ? "./css/login.css" 
-    : menu=="member" 
-    ? "./css/member.css" 
-    : "./css/items.css";
+    document.querySelector("#main-css").href =
+      menu == "home"
+        ? "./css/main.css"
+        : menu == "gallery"
+        ? "./css/gallery.css"
+        : menu == "login"
+        ? "./css/login.css"
+        : menu == "member"
+        ? "./css/member.css"
+        : "./css/items.css";
     // menu값이 "home"인 경우 main.css를 로딩하고
     // menu값이 "gallery"인 경우 gallery.css를 로딩하고
     // menu값이 "login"인 경우 login.css를 로딩하고
@@ -42,9 +44,35 @@ function Layout() {
     // 기타 메뉴인 경우 items.css를 로딩한다!
 
     // 페이지 최상단이동코드
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
 
-  },[menu]);
+    ////////////////////////////////////////////////////////
+    // [ 이벤트의 해제는 removeEventListener()를 사용한다!!! ]
+    // 부드러운 스크롤은 "home"에서만 적용함!
+    if (menu == "home")
+      document.addEventListener("wheel", scrolled, { passive: false });
+    // "home"이 아닌경우는 모두 이벤트를 해재한다!
+    else document.removeEventListener("wheel", scrolled, { passive: false });
+    /////////////////////////////////////////////////////////
+
+    // 슬림적용 대상: #top-area
+    const topMenu = document.querySelector("#top-area");
+
+    // 슬림메뉴 적용하기 : "home"에서만 적용
+    const chkSlim = () => {
+      // 스크롤 위치값 구하기
+      let scTop = window.scrollY;
+      console.log("슬림적용!!!", scTop);
+
+      if (scTop > 200) topMenu.classList.add("on");
+      else topMenu.classList.remove("on");
+    }; //////// chkSlim 함수 /////////
+
+    // 스크롤 이벤트 적용하기 : scroll이벤트
+    // "home"에서만 적용하기
+    if (menu == "home") window.addEventListener("scroll", chkSlim);
+    else window.removeEventListener("scroll", chkSlim);
+  }, [menu]);
 
   // 코드 리턴구역 ////////////
   return (
@@ -52,17 +80,17 @@ function Layout() {
       {/* 1. 상단영역 컴포넌트 */}
       <TopArea changeMenu={setMenu} />
       {/* 2. 메인영역 컴포넌트 */}
-      {
-      menu=="home"
-      ?<MainArea />
-      :menu=="gallery"
-      ?<Gallery />
-      :menu=="login"
-      ?<Login changeMenu={setMenu} />
-      :menu=="member"
-      ?<Member changeMenu={setMenu} />
-      :<ItemsArea catName={menu} />
-      }
+      {menu == "home" ? (
+        <MainArea />
+      ) : menu == "gallery" ? (
+        <Gallery />
+      ) : menu == "login" ? (
+        <Login changeMenu={setMenu} />
+      ) : menu == "member" ? (
+        <Member changeMenu={setMenu} />
+      ) : (
+        <ItemsArea catName={menu} />
+      )}
       {/* 3. 하단영역 컴포넌트 */}
       <FooterArea />
     </React.Fragment>
