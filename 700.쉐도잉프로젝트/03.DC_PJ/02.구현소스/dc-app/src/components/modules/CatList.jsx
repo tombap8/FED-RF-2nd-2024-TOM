9df649238e222071a1da4eed2d85275575e04f2f
Tom B. Parker <tombap8@hotmail.com>
2024-06-13 14:59:29 +0900

    ss

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/CatList.jsx
@@ -0,0 +1,30 @@
+// DC PJ 캐릭터 리스트 컴포넌트
+import React from "react";
+
+// 캐릭터 리스트 CSS
+import "../../css/cat_list.scss";
+/* 
+Root >
+    section.cat-swbox >
+        h2.tit + 스와이퍼 컴포넌트(<SwiperCat />)
+    _______________________________
+
+    -> 스와이퍼 컴포넌트 구조:
+        스와이퍼 리스트 >
+            a링크(<Link>) >
+                section.sw-inbox2 > img
+                +
+                div.cat-tit2 > h3
+*/
+
+function CatList(props) {
+  return (
+    <section className="cat-swbox">
+      {/* 1. 모듈타이틀 */}
+      <h2 className="tit">WHO'S WHO: THE JUSTICE LEAGUE</h2>
+      {/* 2. 스와이퍼 컴포넌트 */}
+    </section>
+  );
+}
+
+export default CatList;
