863cbc462c2d40ba408d355692d2a3d5cd26e7d8
Tom B. Parker <tombap8@hotmail.com>
2024-06-13 11:26:49 +0900

    ss

--- 004.React학습/02.react-ex/js/04.콜백.jsx
+++ 004.React학습/02.react-ex/js/04.콜백.jsx
@@ -2,11 +2,35 @@
 
 // 축구선수에게 좋아요 표시하는 컴포넌트
 function MarkLike() {
-    console.log("MarkLike컴포넌트 랜더링");
+  console.log("MarkLike컴포넌트 랜더링");
   // 세명의 선수에 대한 좋아요 상태관리 변수
   const [son, setSon] = React.useState(false);
   const [danaka, setDanaka] = React.useState(false);
   const [lee, setLee] = React.useState(false);
+
+  // 상태관리변수를 업데이트 하는 함수 ///
+  // useCallback()을 사용하여 변경시에만 업데이트함!
+  const toggleSon = React.useCallback(() => {
+    setSon(!son);
+  }, [son]);
+  const toggleDanaka = React.useCallback(() => {
+    setDanaka(!danaka);
+  }, [danaka]);
+  const toggleLee = React.useCallback(() => {
+    setLee(!lee);
+  }, [lee]);
+
+  // [기존 useCallback사용 안한 일반함수] ///
+  //   const toggleSon = () => {
+  //     setSon(!son);
+  //   };
+  //   const toggleDanaka = () => {
+  //     setDanaka(!danaka);
+  //   };
+  //   const toggleLee = () => {
+  //     setLee(!lee);
+  //   };
+
   /*********************************************** 
       [ 리액트 성능 최적화를 위한 문제인식!!! ]
       -> 하나의 버튼 클릭시 하나만 변경됨에도 불구하고
@@ -38,11 +62,24 @@ function MarkLike() {
       >
         좋아요~! 부탁해요!!
       </h1>
+      {/* 좋아요 버튼 서브 컴포넌트 호출 */}
+      <ShowLike name="손흥민" sts={son} fn={toggleSon} />
+      <ShowLike name="다나카" sts={danaka} fn={toggleDanaka} />
+      <ShowLike name="이강인" sts={lee} fn={toggleLee} />
     </div>
   );
 } ///////////// MarkLike 컴포넌트 ////////////
-/// 좋아요 서브 컴포넌트 /////////
-const ShowLike = ({ name, sts, fn }) => {
+
+/////////////////////////////////////
+/// [ 좋아요 서브 컴포넌트 ] /////////
+// 호출되는 컴포넌트가 매번 리랜더링되므로
+// 메인 컴포넌트의 useCallback처리된 함수도
+// 매번 새로 그려짐! 그래서 효과가 없다!
+// 따라서 호출되는 서브 컴포넌트를 메모이제이션 처리해야
+// useCallback도 효과를 보게된다!!!->>> 중요!!!
+// 방법: 기존 컴포넌트 함수를 React.memo(컴포넌트) 처리
+const ShowLike = React.memo(({ name, sts, fn }) => {
+  console.log("ShowLike컴포넌트 랜더링");
   // name - 선수명 / sts - 상태변수 / fn - 변경함수
   console.log({ name, sts });
   // 코드리턴 ////////////
@@ -53,6 +90,7 @@ const ShowLike = ({ name, sts, fn }) => {
       </button>
     </div>
   );
-}; ///////// ShowLike 컴포넌트 /////////////
+}); ///////// ShowLike 컴포넌트 /////////////
+
 // 컴포넌트 출력하기 ///////
 ReactDOM.render(<MarkLike />, document.querySelector("#root"));
