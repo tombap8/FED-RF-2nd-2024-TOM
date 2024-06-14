39c63783efe555045ae663062dfeb7fc9e27909b
Tom B. Parker <tombap8@hotmail.com>
2024-06-14 15:37:25 +0900

    Update CatDetail.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/CatDetail.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/CatDetail.jsx
@@ -6,7 +6,6 @@ import React from "react";
 // 라우터로 전달한 state값을 읽기위한 객체
 import { useLocation } from "react-router-dom";
 
-
 import Banner from "../modules/Banner";
 import CatList from "../modules/CatList";
 
@@ -15,19 +14,25 @@ import "../../css/cat_detail.scss";
 
 function CatDetail() {
   // 라우터 호출시 전달한 값을 받는다!
+  const loc = useLocation();
+  const cname = loc.state.cname;
+  const cdesc = loc.state.cdesc;
+  const facts = loc.state.facts;
+  console.log(cname, cdesc, facts);
+
   return (
     <>
       {/* 1.배너모듈 */}
-      <Banner catName={"SUPERMAN"} />
+      <Banner catName={cname} />
       {/* 2.상세정보박스 */}
       <div className="detail">
         {/* 2-1.캐릭터 설명박스 */}
         <div className="desc-box">
           {/* 캐릭터명 */}
-          <h2>{"수퍼맨"}</h2>
+          <h2>{cname}</h2>
           {/* 캐릭터소개 */}
           <div className="cdesc">
-            <p>{"수퍼맨은 미국의 뉴욕에서 태어난 캐릭터이다."}</p>
+            <p>{cdesc}</p>
           </div>
         </div>
         {/* 2-2.캐릭터명세 */}
@@ -36,11 +41,11 @@ function CatDetail() {
             <h3>CHARACTER FACTS</h3>
             {/* 테이블로 명세배열만큼 tr을 만들어준다! */}
             <table>
-                <tbody>
-                    <tr>
-                        <td>명세</td>
-                    </tr>
-                </tbody>
+              <tbody>
+                <tr>
+                  <td>{facts}</td>
+                </tr>
+              </tbody>
             </table>
           </div>
         </div>
