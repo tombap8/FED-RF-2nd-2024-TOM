c6a1ad998f1d62b0849b68ad319b90279f8651f1
Tom B. Parker <tombap8@hotmail.com>
2024-06-17 13:14:39 +0900

    ss

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/TopArea.jsx
@@ -2,8 +2,50 @@ import React from "react";
 
 // 메뉴 데이터 불러오기
 import { gnbData } from "../../js/data/gnb";
+import { TotalMenu } from "../modules/TotalMenu";
+// 제이쿼리
+import $ from "jquery";
 
 function TopArea(props) {
+
+  // 전체 메뉴 열기닫기 함수 ////
+  const showHideMenu = (e)=>{
+    // console.log(e.currentTarget);
+
+    // 전체 메뉴대상 : .mbox
+    // 1. 메뉴 보이기 숨기기 전환하기
+    $(".mbox").fadeToggle(300);
+    // fadeIn() 서서히 나타남
+    // fadeOut() 서서히 사라짐 -> display:none
+    // fadeToggle() 서서히 나타남/사라짐 전환
+
+    // 2. 햄버거 버튼에 클래스 "on"넣기/빼기
+    $(e.currentTarget).toggleClass("on");
+    // addClass() 클래스넣기
+    // removeClass() 클래스빼기
+    // toggleClass() 클래스넣기/빼기
+
+    // console.log($(e.currentTarget).is(".on"));
+
+    // 3. 비디오 재생/멈춤하기
+    // 대상: .bgm
+    let bgm = $(".bgm");
+    // 제이쿼리의 미디어를 선택후 실제 사용할때는
+    // get(0)하고 난 후 사용한다!
+
+    // 햄버거버튼에 클래스"on"있으면 재생 없으면 멈춤 
+    $(e.currentTarget).is(".on")?
+    bgm.get(0).play()
+    : bgm.get(0).pause();
+    // play()는 재생, pause()는 멈춤 메서드
+
+    // 비교해서 JS는 선택후 바로 play(), pause()를 사용함
+    // document.querySelector(".bgm").play();
+    // document.querySelector(".bgm").pause();
+
+  }; ////////// showHideMenu /////////////
+
+  // 코드 리턴구역 //////////////
   return (
     <>
       <div id="top-area">
@@ -23,10 +65,11 @@ function TopArea(props) {
               ))}
             </ul>
           </nav>
-          <div className="ham">
+          <div className="ham" onClick={showHideMenu}>
             <span></span> <span></span> <span></span>
           </div>
           {/* 전체메뉴 컴포넌트 */}
+          <TotalMenu />
         </header>
       </div>
     </>
---
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/TotalMenu.jsx
@@ -0,0 +1,70 @@
+// Pilot PJ 전체메뉴 컴포넌트
+
+export function TotalMenu() {
+  // 코드 리턴 //////////////////////
+  return (
+    <>
+      <div className="mbox">
+        <video
+          src="./images/disc2018.mp4"
+          loop="loop"
+          muted="muted"
+          className="bgm"
+        ></video>
+        <nav className="mlist">
+          <dl>
+            <dt>
+              <a href="#">MEN</a>
+            </dt>
+            <dd>
+              <a href="#">T-SHIRT</a>
+            </dd>
+            <dd>
+              <a href="#">JACKET</a>
+            </dd>
+            <dd>
+              <a href="#">TRAINING WARE</a>
+            </dd>
+            <dd>
+              <a href="#">BEACH WARE</a>
+            </dd>
+          </dl>
+          <dl>
+            <dt>
+              <a href="#">WOMEN</a>
+            </dt>
+            <dd>
+              <a href="#">T-SHIRT</a>
+            </dd>
+            <dd>
+              <a href="#">JACKET</a>
+            </dd>
+            <dd>
+              <a href="#">TRAINING WARE</a>
+            </dd>
+            <dd>
+              <a href="#">BEACH WARE</a>
+            </dd>
+          </dl>
+          <dl>
+            <dt>
+              <a href="#">STYLE</a>
+            </dt>
+            <dd>
+              <a href="#">COLLECTION</a>
+            </dd>
+            <dd>
+              <a href="#">SEASON AD</a>
+            </dd>
+            <dd>
+              <a href="#">STAR &amp; NEWS</a>
+            </dd>
+            <dd>
+              <a href="#">MAIN ITEM</a>
+            </dd>
+          </dl>
+        </nav>
+      </div>
+    </>
+  );
+} ///////// TotalMenu 컴포넌트 //////////
