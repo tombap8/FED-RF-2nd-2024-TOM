a30f46b63642a3621c88cab3e14615464e58461b
Tom B. Parker <tombap8@hotmail.com>
2024-06-19 11:15:15 +0900

    ss

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/FashionIntro.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/FashionIntro.jsx
@@ -1,5 +1,8 @@
 // 패션 인트로 컴포넌트 ////
-import React from "react";
+import React, { useContext } from "react";
+
+// 컨텍스트 API 불러오기
+import { pCon } from "./pCon";
 
 // 데이터 불러오기
 import { fsData } from "../../js/data/fashion_intro";
@@ -13,6 +16,9 @@ function FashionIntro({ catName, subCat, opt }) {
   //  (서브가 아닌경우 subCat의 값은 "etc"임!)
   // 3. opt - 방향옵션(역방향은 true / 정방향은 false)
   //  (역방향은 flex-direction: row-reverse 적용!)
+
+  // 컨텍스트 API사용하기
+  const myCon = useContext(pCon);
 
   // 선택 데이터 변수할당
   const selData = fsData[catName];
@@ -36,7 +42,10 @@ function FashionIntro({ catName, subCat, opt }) {
             {/* (참고) 데이터에 태그가 있어서 이를 html로 넣으려면
             dangerouslySetInnerHTML={{__html:데이터}} 
             속성을 사용한다! */}
-            <a href="#">
+            <a href="#" onClick={(e)=>{
+              e.preventDefault();
+              myCon.setPgName("fashion");
+            }}>
               {selData.tit[0][0]}
               <br />
               {selData.tit[0][1]}
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -4,7 +4,8 @@ import TopArea from './components/layout/TopArea';
 import MainArea from './components/layout/MainArea';
 import FooterArea from './components/layout/FooterArea';
 
-
+// 컨텍스트 API 불러오기
+import { pCon } from './components/modules/pCon';
 
 // 전체 공통 CSS
 import "./css/index.scss";
@@ -14,14 +15,18 @@ function MainComponent(props) {
   // 1. 페이지변경 상태변수
   const [pgName, setPgName] = useState("main");
 
+  /**************************************** 
+    [ 컨텍스트 API 공개 변수들 ]
+    1. setPgName - 페이지이름 업데이트메서드
+  ****************************************/
 
   // 코드 리턴구역 /////////////
   return (
-    <>
+    <pCon.Provider value={{setPgName}}>
       <TopArea />
-      <MainArea page="main" />
+      <MainArea page={pgName} />
       <FooterArea />
-    </>
+    </pCon.Provider>
   );
 }
 
