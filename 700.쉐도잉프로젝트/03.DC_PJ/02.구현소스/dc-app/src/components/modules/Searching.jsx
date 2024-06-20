bd8a8f8787df140bb0c8c9b8bea9eb5e028b08c5
Tom B. Parker <tombap8@hotmail.com>
2024-06-20 12:16:54 +0900

    Update Searching.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Searching.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Searching.jsx
@@ -47,6 +47,8 @@ function Searching({kword}) {
         // filter에서 변수에 저장할 배열로 수집된다!
     }); //////////////// filter ///////////////////
 
+    // [ 결과내 재검색 : 데이터 항목중 alignment값으로 검색함! ]
+
     // [ 정렬기능 추가하기 ] /////////
     // (1) 오름차순일 경우
     if(sort == "asc"){
@@ -103,7 +105,14 @@ function Searching({kword}) {
             // 검색어 상태변수만 업데이트하면 끝!!!
             // -> setKw(검색어)
             onKeyUp={(e)=>{
-              if(e.key=="Enter") setKw(e.target.value);
+              if(e.key=="Enter"){ 
+                // 검색어 상태값 변경
+                setKw(e.target.value);
+                // 처음 검색시 정렬은 기본정렬 오름차순(asc)
+                setSort("asc");
+                // 정렬선택박스 선택값변경(DOM에서 보이기변경)
+                document.querySelector("#sel").value = "asc";
+              } /// if ///
             }}
             />
           </div>
@@ -121,7 +130,15 @@ function Searching({kword}) {
                   <li>
                     Heroes
                     {/* 숨긴 체크박스 */}
-                    <input type="checkbox" id="hero" className="chkhdn" />
+                    <input type="checkbox" id="hero" className="chkhdn"
+                    // 체크변경시 change이벤트발생
+                    onChange={(e)=>{
+                      // 체크박스의 checked속성은
+                      // 체크시 true, 불체크시 false리턴
+                      console.log(e.target.checked);
+                    }}
+
+                    />
                     {/* 디자인노출 라벨 */}
                     <label htmlFor="hero" className="chklb"></label>
                   </li>
