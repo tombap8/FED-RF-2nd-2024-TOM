ee6391f19623d1b8e884d1ffbc828ce78615631a
Tom B. Parker <tombap8@hotmail.com>
2024-07-03 15:21:54 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -16,31 +16,62 @@ import "../../css/board.scss";
 import "../../css/board_file.scss";
 
 export default function Board() {
-
-    /********************************************** 
+  /********************************************** 
         함수명: bindList
         기능 : 페이지별 리스트를 생성하여 바인딩함
     **********************************************/
-   const bindList = () => {
+  const bindList = () => {
     // console.log(baseData);
 
-    return baseData.map(v=>
-        <tr>
-            <td>{v.idx}</td>
-            <td>
-              <a href="#" data-idx="51">
-                {v.cont}
-              </a>
-            </td>
-            <td>{v.unm}</td>
-            <td>{v.date}</td>
-            <td>{v.cnt}</td>
-          </tr>
-        )
-    
+    // 1. 전체 원본데이터 선택
+    let orgData = baseData;
 
-   }; /////////// bindList 함수 /////////////////
+    // 2. 정렬 적용하기 : 내림차순
+    orgData.sort((a, b) =>
+      Number(a.idx) > Number(b.idx) ? -1 : Number(a.idx) < Number(b.idx) ? 1 : 0
+    );
 
+    // 3. 일부 데이터만 선택
+    // 예시로 0번부터 9번까지만 선택
+    // 한페이지당 10개라면...
+    // 페이지 번호와 연관시켜 본다!
+    // 1,2,3,4,...
+    // 시작번호 = (페이지번호-1)*단위수
+    // 끝번호 = 페이지번호*단위수
+
+    // 페이지 번호
+    const pageNum = 3;
+    // 페이지당 개수
+    const unitSize = 10;
+
+    const selData = [];
+    for (
+      // 시작값
+      let i = (pageNum - 1) * unitSize;
+      // 한계값
+      i < pageNum * unitSize;
+      // 증감
+      i++
+    ) {
+      selData.push(orgData[i]);
+    }
+
+    console.log("일부데이터:", selData);
+
+    return selData.map((v, i) => (
+      <tr key={i}>
+        <td>{i + 1}</td>
+        <td>
+          <a href="#" data-idx="51">
+            {v.cont}
+          </a>
+        </td>
+        <td>{v.unm}</td>
+        <td>{v.date}</td>
+        <td>{v.cnt}</td>
+      </tr>
+    ));
+  }; /////////// bindList 함수 /////////////////
 
   //// 코드 리턴구역 //////////////
   return (
@@ -69,9 +100,7 @@ export default function Board() {
             <th>Hits</th>
           </tr>
         </thead>
-        <tbody>
-            {bindList()}
-        </tbody>
+        <tbody>{bindList()}</tbody>
         <tfoot>
           <tr>
             <td colSpan="5" className="paging">
