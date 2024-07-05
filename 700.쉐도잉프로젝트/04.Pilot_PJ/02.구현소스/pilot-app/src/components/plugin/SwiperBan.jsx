bd9cec9136e0f0e509b0ec69b1ec5f35c61d4495
Tom B. Parker <tombap8@hotmail.com>
2024-07-05 11:51:20 +0900

    Update SwiperBan.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/plugin/SwiperBan.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/plugin/SwiperBan.jsx
@@ -87,13 +87,50 @@ export function SwiperBan({ cat }) {
     return temp;
   }; ///////////// makeList 함수 //////////
 
-  // 소멸자 만들기 ////////////
+  // 화면랜더링 구역 : 한번만 /////////////
   useEffect(()=>{
+
+    // 스와이퍼 객체 : ref로 외부에 노출한 스와이퍼객체
+    let objSwp = swpObj.current.swiper;
+
+    // 화면절반크기 기준값
+    const winCta = window.innerHeight / 2;
+    // 스크롤시 호출함수
+    const scrollFn = () => {
+        if (window.scrollY > winCta) {
+            // 영상플레이시 자동넘김 끄기
+            objSwp.autoplay.stop();
+            objSwp.autoplay.running = false;
+            endObj.current = true;
+            // 영상멈추기
+            mvEle.pause();
+        } else {
+            // 자동넘김 시작
+            objSwp.autoplay.start();
+            // 자동넘김 속성 true전환!
+            objSwp.autoplay.running = true;
+            endObj.current = false;
+            // 영상재생
+            mvEle.play();
+        }
+    };
+
+    // 스크롤 이동시 기준값에 따라 동영상 재생/멈춤
+    // 단, 동영상객체가 있을때만 걸어준다!
+    if(mvEle)
+    window.addEventListener("scroll", scrollFn);
+
+
+    // [ 소멸자 만들기 ] ////////////
     return(()=>{
       // 동영상변수가 null이 아닐때만 이벤트삭제
-      if(mvEle)
-      mvEle.removeEventListener("timeupdate", actionVideo); 
-    
+      if(mvEle){
+        // 동영상 시간업데이트 이벤트 설정제거
+        mvEle.removeEventListener("timeupdate", actionVideo); 
+        // 스크롤 이벤트 설정제거
+        window.removeEventListener("scroll", scrollFn);
+      } /// if /////
+
       console.log("난 스와이퍼 소멸자!!!");
     });
   },[]); /////// useEffect //////////
