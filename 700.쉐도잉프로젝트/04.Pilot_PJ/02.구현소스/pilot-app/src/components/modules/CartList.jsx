9beb614d81ad450b75b86694e05c6433d2193692
Tom B. Parker <tombap8@hotmail.com>
2024-07-10 11:21:44 +0900

    Update CartList.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/CartList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/CartList.jsx
@@ -3,21 +3,28 @@ import React, { useContext } from "react";
 // 카트 리스트 CSS
 import "../../css/cart_list.scss";
 import { pCon } from "./pCon";
+import { addComma } from "../../js/func/common_fn";
 
 function CartList(props) {
+  // 컨텍스트 사용
+  const myCon = useContext(pCon);
 
-    // 컨텍스트 사용
-    const myCon = useContext(pCon);
+  // 로컬스 데이터 가져오기
+  const selData = JSON.parse(localStorage.getItem("cart-data"));
+  console.log("로컬스:", selData);
 
-    ///// 코드리턴구역 /////////////
+  ///// 코드리턴구역 /////////////
   return (
-    <section id="cartlist" style={{right: "0px"}}>
-      <a href="#" className="cbtn cbtn2"
-      onClick={e=>{
-        e.preventDefault();
-        // 카트 상태값 업데이트
-        myCon.setCartSts(false);
-      }}>
+    <section id="cartlist" style={{ right: "0px" }}>
+      <a
+        href="#"
+        className="cbtn cbtn2"
+        onClick={(e) => {
+          e.preventDefault();
+          // 카트 상태값 업데이트
+          myCon.setCartSts(false);
+        }}
+      >
         <span>닫기버튼</span>
       </a>
       <table>
@@ -35,36 +42,63 @@ function CartList(props) {
             <th>합계</th>
             <th>삭제</th>
           </tr>
-          {/* 카트데이터 연동파트 */}
-          <tr>
-            <td>
-              <img src={process.env.PUBLIC_URL+"/images/goods/women/m8.png"} alt="item" />
-            </td>
-            <td>6</td>
-            <td>[여성]베이직 솔리드 래쉬가드</td>
-            <td>DMSW15731-BK </td>
-            <td>49,000원</td>
-            <td className="cnt-part">
-              <div>
-                <span>
-                  <input type="text" className="item-cnt" readOnly="" defaultValue="1" />
-                  <button className="btn-insert" data-idx="20">
-                    반영
-                  </button>
-                  <b className="btn-cnt">
-                    <img src={process.env.PUBLIC_URL+"/images/cnt_up.png"} alt="증가" />
-                    <img src={process.env.PUBLIC_URL+"/images/cnt_down.png"} alt="감소" />
-                  </b>
-                </span>
-              </div>
-            </td>
-            <td>49,000원</td>
-            <td>
-              <button className="cfn" data-idx="20">
-                ×
-              </button>
-            </td>
-          </tr>          
+          {/* 카트데이터 연동파트
+          ************************** 
+            [데이터 구조정의]
+            1. num : 카트리스트 순번
+            2. idx : 상품고유번호
+            3. cat : 카테고리
+            4. ginfo : 상품정보
+            5. cnt : 상품개수
+          **************************          
+          */}
+          {selData.map((v) => (
+            <tr>
+              <td>
+                <img
+                  src={
+                    process.env.PUBLIC_URL + 
+                    `/images/goods/${v.cat}/${v.ginfo[0]}.png`}
+                  alt="item"
+                />
+              </td>
+              <td>{v.num}</td>
+              <td>{v.ginfo[1]}</td>
+              <td>{v.ginfo[2]} </td>
+              <td>{addComma(v.ginfo[3])}원</td>
+              <td className="cnt-part">
+                <div>
+                  <span>
+                    <input
+                      type="text"
+                      className="item-cnt"
+                      readOnly=""
+                      value={v.cnt}
+                    />
+                    <button className="btn-insert" data-idx="20">
+                      반영
+                    </button>
+                    <b className="btn-cnt">
+                      <img
+                        src={process.env.PUBLIC_URL + "/images/cnt_up.png"}
+                        alt="증가"
+                      />
+                      <img
+                        src={process.env.PUBLIC_URL + "/images/cnt_down.png"}
+                        alt="감소"
+                      />
+                    </b>
+                  </span>
+                </div>
+              </td>
+              <td>{addComma(v.ginfo[3]*v.cnt)}원</td>
+              <td>
+                <button className="cfn" data-idx="20">
+                  ×
+                </button>
+              </td>
+            </tr>
+          ))}
           <tr>
             <td colSpan="6">총합계 :</td>
             <td>662,000원</td>
