8d90ba37f83dcbca380d6257bf61d09176ae7acf
Tom B. Parker <tombap8@hotmail.com>
2024-07-12 11:00:20 +0900

    Update CartList.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/CartList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/CartList.jsx
@@ -18,7 +18,7 @@ function CartList(props) {
 
   // 전체 데이터 개수
   const dataCnt = selData.length;
-  console.log("데이터수:",dataCnt);
+  console.log("데이터수:", dataCnt);
 
   // 총합계함수 /////////////
   const totalFn = () => {
@@ -27,8 +27,8 @@ function CartList(props) {
 
     let result = 0;
 
-    $(".sum-num2").each((idx,ele)=>{
-      console.log("값:",$(ele).val());
+    $(".sum-num2").each((idx, ele) => {
+      console.log("값:", $(ele).val());
       // 숫자로 변환후 기존값에 더하기함!
       result += Number($(ele).val());
     });
@@ -38,20 +38,20 @@ function CartList(props) {
   }; ////////// totalFn ///////////
 
   // 화면랜더링 구역 : dataCnt의존성 /////////
-  useEffect(()=>{
+  useEffect(() => {
     console.log("dataCnt의존성");
     // 카트버튼 나타나기
-      $("#mycart").removeClass("on")
-      .delay(500)//애니메이션 지연시간
-      .fadeIn(300, function(){
+    $("#mycart")
+      .removeClass("on")
+      .delay(500) //애니메이션 지연시간
+      .fadeIn(300, function () {
         // 나타난후 클래스 넣으면 오른쪽이동+작아짐
         $(this).addClass("on");
       }); ///// fadeIn /////
-      
-      // 총합계 찍기 : 3자리마다 콤마함수호출도함
-      $(".total-num").text(addComma(totalFn()));
-      
-  },[dataCnt]); //-> 숫자값은 값할당이므로 변함없음!
+
+    // 총합계 찍기 : 3자리마다 콤마함수호출도함
+    $(".total-num").text(addComma(totalFn()));
+  }, [dataCnt]); //-> 숫자값은 값할당이므로 변함없음!
   // },[selData]); //-> 리랜더링시 객체주소값이 변경되어
   // 매번 새로운값이 업데이트 되기때문에 부적격임!
 
@@ -69,9 +69,12 @@ function CartList(props) {
           onClick={(e) => {
             e.preventDefault();
             // 오른쪽으로 이동하여 사라짐
-            $("#cartlist").animate({
-              right:"-60vw"
-            },400)
+            $("#cartlist").animate(
+              {
+                right: "-60vw",
+              },
+              400
+            );
           }}
         >
           <span>닫기버튼</span>
@@ -79,14 +82,14 @@ function CartList(props) {
         <table>
           {/* 항목별 세로 비율설정 */}
           <colgroup>
-            <col span="1" style={{width: "5%"}}/>
-            <col span="1" style={{width: "8%"}}/>
-            <col span="1" style={{width: "38%"}}/>
-            <col span="1" style={{width: "14%"}}/>
-            <col span="1" style={{width: "10%"}}/>
-            <col span="1" style={{width: "8%"}}/>
-            <col span="1" style={{width: "11%"}}/>
-            <col span="1" style={{width: "5%"}}/>
+            <col span="1" style={{ width: "5%" }} />
+            <col span="1" style={{ width: "8%" }} />
+            <col span="1" style={{ width: "38%" }} />
+            <col span="1" style={{ width: "14%" }} />
+            <col span="1" style={{ width: "10%" }} />
+            <col span="1" style={{ width: "8%" }} />
+            <col span="1" style={{ width: "11%" }} />
+            <col span="1" style={{ width: "5%" }} />
           </colgroup>
           {/* 테이블 제목 */}
           <caption>
@@ -118,13 +121,15 @@ function CartList(props) {
                     width: "100%",
                   }}
                 >
-              {/* 내부용 스크롤되는 테이블 */}
-              <table style={{
-                margin:"0",
-                width:"100%"
-              }}>
-                <tbody>
-            {/* 카트데이터 연동파트
+                  {/* 내부용 스크롤되는 테이블 */}
+                  <table
+                    style={{
+                      margin: "0",
+                      width: "100%",
+                    }}
+                  >
+                    <tbody>
+                      {/* 카트데이터 연동파트
             **************************
               [데이터 구조정의]
               1. idx : 상품고유번호
@@ -133,109 +138,154 @@ function CartList(props) {
               4. cnt : 상품개수
             **************************
             */}
-              {selData.map((v, i) => (
-                <tr key={i}>
-                  {/* 일련번호 */}
-                  <td>{i+1}</td>
-                  {/* 상품이미지 */}
-                  <td>
-                    <img
-                      src={
-                        process.env.PUBLIC_URL +
-                        `/images/goods/${v.cat}/${v.ginfo[0]}.png`
-                      }
-                      alt="item"
-                    />
-                  </td>
-                  <td>{v.ginfo[1]}</td>
-                  <td>{v.ginfo[2]} </td>
-                  <td>{addComma(v.ginfo[3])}원</td>
-                  <td className="cnt-part">
-                    <div>
-                      <span>
-                        <input
-                          type="text"
-                          className="item-cnt"
-                          readOnly
-                          value={v.cnt}
-                          onChange={() => {}}
-                        />
-                        <button className="btn-insert" data-idx="20">
-                          반영
-                        </button>
-                        <b className="btn-cnt">
-                          <img
-                            src={
-                              process.env.PUBLIC_URL +
-                              "/images/cnt_up.png"
-                            }
-                            alt="증가"
-                          />
-                          <img
-                            src={
-                              process.env.PUBLIC_URL +
-                              "/images/cnt_down.png"
-                            }
-                            alt="감소"
-                          />
-                        </b>
-                      </span>
-                    </div>
-                  </td>
-                  <td>
-                    <span
-                    className="sum-num1">{addComma(v.ginfo[3] * v.cnt)}</span>원
-                    {/* 계산된 합계금액 숫자만 히든필드에 넣어놓고 총합계 계산에 사용함! */}
-                    <input
-                      className="sum-num2"
-                      type="hidden"
-                      defaultValue={v.ginfo[3] * v.cnt}
-                    />
-                  </td>
-                  <td>
-                    {/* 데이터 삭제기능 버튼 */}
-                    <button className="cfn" 
-                    onClick={()=>{
-                      // confirm()의 "확인"클릭시 true
-                      if(window.confirm("정말정말정말로 지우시겠습니까? 할인도하는데???")){
-                        // console.log("삭제함!!!");
-                        // console.log("현재객체:",selData);
-                        // console.log("지울순번:",i);
-                        // // splice 자체를 찍으면 지워진 요소가 찍힘
-                        // console.log("지우기:",selData.splice(i,1));
+                      {selData.map((v, i) => (
+                        <tr key={i}>
+                          {/* 일련번호 */}
+                          <td>{i + 1}</td>
+                          {/* 상품이미지 */}
+                          <td>
+                            <img
+                              src={
+                                process.env.PUBLIC_URL +
+                                `/images/goods/${v.cat}/${v.ginfo[0]}.png`
+                              }
+                              alt="item"
+                            />
+                          </td>
+                          <td>{v.ginfo[1]}</td>
+                          <td>{v.ginfo[2]} </td>
+                          <td>{addComma(v.ginfo[3])}원</td>
+                          <td className="cnt-part">
+                            <div>
+                              <span>
+                                <input
+                                  type="text"
+                                  className="item-cnt"
+                                  readOnly
+                                  value={v.cnt}
+                                  onChange={() => {}}
+                                />
+                                <button 
+                                className="btn-insert" 
+                                onClick={(e)=>{}}
+                                >
+                                  반영
+                                </button>
+                                <b
+                                  className="btn-cnt"
+                                  onClick={(e) => {
+                                    // 업데이트 대상(input박스)
+                                    let tg = $(e.currentTarget).siblings(
+                                      "input"
+                                    );
 
-                        // 지울 배열 순번은 map()에서 i로 들어옴
-                        // 지울 배열은 selData임
-                        // 1.데이터 지우기 : 
-                        selData.splice(i,1);
+                                    // 하위 클릭된 이미지 종류파악하기
+                                    // e.target으로 설정하여 하위요소인
+                                    // 이미지가 선택되게 해준다!
+                                    // e.currentTarget은 이벤트가 걸린
+                                    // 요소 자신이다!(비교할것!)
+                                    let btnAlt = $(e.target).attr("alt");
+                                    console.log(btnAlt);
+                                    // 증가감소 분기하여 숫자변경반영하기
+                                    if (btnAlt == "증가") {
+                                      // tg값을 읽어와서 1을 더한다!
+                                      tg.val(Number(tg.val()) + 1);
+                                    } //// if ///////
+                                    else if (btnAlt == "감소") {
+                                      // tg값을 읽어와서 1을 뺀다!
+                                      // 단, 1보다 작아지지 않게 한다!
+                                      tg.val(
+                                        Number(tg.val()) == 1
+                                          ? 1
+                                          : Number(tg.val()) - 1
+                                      );
+                                    } ///// else if ////////
 
-                        // 2. 데이터 문자화하기 : 변경된 원본을 문자화
-                        let res = JSON.stringify(selData);
+                                    // 클릭시 반영버튼 나타나기
+                                    $(e.currentTarget).siblings(".btn-insert")
+                                    .css({width:"auto"});
 
-                        // 3.로컬스 "cart-data"반영하기
-                        localStorage.setItem("cart-data",res);
 
-                        // 4. 카트리스트 전역상태변수 변경
-                        myCon.setLocalsCart(res);
 
-                        // 5. 데이터개수가 0이면 카트리스트
-                        // 상태변수를 false로 변경하여 
-                        // 카트리스트 출력을 없앤다!
-                        if(selData.length == 0) 
-                          myCon.setCartSts(false);
-                        
-                        // let aa = [];
-                        // aa.splice(지울순번,지울개수)
+                                  }}
+                                >
+                                  <img
+                                    src={
+                                      process.env.PUBLIC_URL +
+                                      "/images/cnt_up.png"
+                                    }
+                                    alt="증가"
+                                  />
+                                  <img
+                                    src={
+                                      process.env.PUBLIC_URL +
+                                      "/images/cnt_down.png"
+                                    }
+                                    alt="감소"
+                                  />
+                                </b>
+                              </span>
+                            </div>
+                          </td>
+                          <td>
+                            <span className="sum-num1">
+                              {addComma(v.ginfo[3] * v.cnt)}
+                            </span>
+                            원
+                            {/* 계산된 합계금액 숫자만 히든필드에 넣어놓고 총합계 계산에 사용함! */}
+                            <input
+                              className="sum-num2"
+                              type="hidden"
+                              defaultValue={v.ginfo[3] * v.cnt}
+                            />
+                          </td>
+                          <td>
+                            {/* 데이터 삭제기능 버튼 */}
+                            <button
+                              className="cfn"
+                              onClick={() => {
+                                // confirm()의 "확인"클릭시 true
+                                if (
+                                  window.confirm(
+                                    "정말정말정말로 지우시겠습니까? 할인도하는데???"
+                                  )
+                                ) {
+                                  // console.log("삭제함!!!");
+                                  // console.log("현재객체:",selData);
+                                  // console.log("지울순번:",i);
+                                  // // splice 자체를 찍으면 지워진 요소가 찍힘
+                                  // console.log("지우기:",selData.splice(i,1));
 
-                        
-                      }//// if /////
-                    }}
-                    >
-                      ×
-                    </button>
-                  </td>
-                </tr>
-              ))}
+                                  // 지울 배열 순번은 map()에서 i로 들어옴
+                                  // 지울 배열은 selData임
+                                  // 1.데이터 지우기 :
+                                  selData.splice(i, 1);
+
+                                  // 2. 데이터 문자화하기 : 변경된 원본을 문자화
+                                  let res = JSON.stringify(selData);
+
+                                  // 3.로컬스 "cart-data"반영하기
+                                  localStorage.setItem("cart-data", res);
+
+                                  // 4. 카트리스트 전역상태변수 변경
+                                  myCon.setLocalsCart(res);
+
+                                  // 5. 데이터개수가 0이면 카트리스트
+                                  // 상태변수를 false로 변경하여
+                                  // 카트리스트 출력을 없앤다!
+                                  if (selData.length == 0)
+                                    myCon.setCartSts(false);
+
+                                  // let aa = [];
+                                  // aa.splice(지울순번,지울개수)
+                                } //// if /////
+                              }}
+                            >
+                              ×
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
                     </tbody>
                   </table>
                 </div>
@@ -246,7 +296,9 @@ function CartList(props) {
           <tfoot>
             <tr>
               <td colSpan="6">총합계 :</td>
-              <td><span className="total-num"></span>원</td>
+              <td>
+                <span className="total-num"></span>원
+              </td>
               <td></td>
             </tr>
             <tr>
@@ -258,20 +310,25 @@ function CartList(props) {
         </table>
       </section>
       {/* 카트버튼이미지 박스 */}
-      <div id="mycart"
-      onClick={(e) => {
-        e.preventDefault();
-        // 왼쪽으로 이동하여 나타남!
-        $("#cartlist").animate({
-          right:"0"
-        },400)
-      }}
+      <div
+        id="mycart"
+        onClick={(e) => {
+          e.preventDefault();
+          // 왼쪽으로 이동하여 나타남!
+          $("#cartlist").animate(
+            {
+              right: "0",
+            },
+            400
+          );
+        }}
       >
         {/* 카트이미지 */}
-        <img 
-        src={process.env.PUBLIC_URL+"/images/mycart.gif"} 
-        title={dataCnt+"개의 상품이 있습니다"}
-        alt="장바구니" />
+        <img
+          src={process.env.PUBLIC_URL + "/images/mycart.gif"}
+          title={dataCnt + "개의 상품이 있습니다"}
+          alt="장바구니"
+        />
         {/* 카트상품개수 출력박스 */}
         <div className="cntBx">{dataCnt}</div>
       </div>
