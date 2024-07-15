30423132b546f534358af544b839f99d636fd265
Tom B. Parker <tombap8@hotmail.com>
2024-07-15 13:15:44 +0900

    Update Board.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -33,6 +33,13 @@ export default function Board() {
   // 로컬스 데이터 변수할당하기!
   const baseData = JSON.parse(localStorage.getItem("board-data"));
 
+  
+  // 원본 데이터에 정렬 적용하기 : 내림차순
+  baseData.sort((a, b) =>
+    Number(a.idx) > Number(b.idx) ? -1 : Number(a.idx) < Number(b.idx) ? 1 : 0
+  );
+
+
   // [ 상태관리 변수 ] ///
   // [1] 페이지 번호
   const [pageNum, setPageNum] = useState(1);
@@ -268,7 +275,63 @@ export default function Board() {
       // -> 모드변경! "L"
       setMode("L");
     } /// if ///
+
     // 3. 수정모드 서브밋 (mode=="M")
+    else if (mode == "M") {
+      // 0.현재 로그인 사용자 정보 파싱하기
+      let person = JSON.parse(sts);
+
+      // 1. 오늘날짜 생성하기 /////
+      // -> 수정시 수정날짜 항목을 새로 만들고 입력함!
+      let today = new Date();
+      // yy-mm-dd 형식으로 구하기
+      // 제이슨 날짜형식 : toJSON()
+      // ISO 표준형식 : toISOString()
+      // 시간까지 나오므로 앞에 10자리만 가져감!
+      // 문자열.substr(0,10)
+
+      // 2. 현재 데이터 idx값
+      let currIdx = selRecord.current.idx;
+
+      // 3. 기존 데이터로 찾아서 변경하기
+      // : 로컬스 데이터 -> baseData
+      // find()는 특정항목을 찾아서 리턴하여 데이터를 가져
+      // 오기도 하지만 업데이트 등 작업도 가능함!
+      baseData.find(v=>{
+        // console.log(v,selRecord);
+        if(v.idx == currIdx){
+          // [ 업데이트 작업하기 ]
+          // 기존항목변경 : tit, cont
+          // 이미 선택된 selRecord 참조변수의 글번호인 idx로
+          // 원본 데이터를 조회하여 기존 데이터를 업데이트함!
+
+          // (1) 글제목 : tit
+          v.tit = title;    
+          // (2) 글내용 : cont
+          v.cont = cont;
+          // 추가항목 
+          // (원래는 확정된 DB스키마에 따라 입력해야하지만
+          // 우리가 사용하는 로컬스토리지의 확장성에 따라 필요한
+          // 항목을 추가하여 넣는다!)
+          // (3) 수정일 : mdate
+          v.mdate = today.toJSON().substr(0,10);
+
+          // 해당항목을 만나면 끝남!
+          return true;
+        } /// if ///
+      }); /////// find 메서드 /////////  
+
+      // 4. 로컬스에 업데이트하기 //////
+      localStorage.setItem("board-data", 
+      JSON.stringify(baseData));
+
+      // 로컬스 확인!
+      // console.log(localStorage.getItem("board-data"));
+
+      // 5. 리스트로 돌아가기 /////
+      // -> 모드변경! "L"
+      setMode("L");
+    }
   }; ////////// submitFn //////////////
 
   //// 코드 리턴구역 //////////////
