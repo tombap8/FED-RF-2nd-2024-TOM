6df1b95578d152bf26826e4b310df7f29f2d0f22
Tom B. Parker <tombap8@hotmail.com>
2024-07-26 16:15:10 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -41,15 +41,24 @@ export default function Board() {
   ///////// [ 상태관리 변수 ] //////////////
   // [1] 페이지 번호
   const [pageNum, setPageNum] = useState(1);
+
   // [2] 기능모드
   const [mode, setMode] = useState("L");
   // (1) 리스트 모드(L) : List Mode
   // (2) 글보기 모드(R) : Read Mode
   // (3) 글쓰기 모드(W) : Write Mode
   // (4) 수정 모드(M) : Modify Mode (삭제포함)
+
   // [3] 검색어 저장변수 : 배열 [기준,검색어]
   const [keyword, setKeyword] = useState(["", ""]);
   console.log("[기준,키워드]", keyword);
+
+  // [4] 정렬 기준값 상태변수 : 값 (asc(-1) / desc(1))
+  const [sort, setSort] = useState(1);
+  // -> 기존 셋팅값에 1을 곱하면 원래값, -1을 곱하면 반대값셋팅
+
+  // [5] 정렬 항목값 상태변수 : 값 - idx / tit
+  const [sortCta, setSortCta] = useState("idx");
 
   // [ 참조변수 ] ///
   // [1] 전체 개수 - 매번 계산하지 않도록 참조변수로!
@@ -101,8 +110,19 @@ export default function Board() {
     totalCount.current = orgData.length;
 
     // 2. 정렬 적용하기 : 내림차순
+    // sort값이 1이면 desc(현재상태유지)
+    // sort값이 -1이면 asc(부호반대변경)
+    // 정렬항목은 sortCta값에 따름("idx"/"tit")
+
+    // "idx"정렬항목일 경우만 Number()처리함수
+    const chgVal = x => 
+    sortCta=="idx"?Number(x[sortCta]):x[sortCta];
+
     orgData.sort((a, b) =>
-      Number(a.idx) > Number(b.idx) ? -1 : Number(a.idx) < Number(b.idx) ? 1 : 0
+      chgVal(a) > chgVal(b) 
+      ? -1 * sort 
+      : chgVal(a) < chgVal(b) 
+      ? 1 * sort : 0
     );
 
     // 3. 일부 데이터만 선택
@@ -382,6 +402,8 @@ export default function Board() {
             pgPgNum={pgPgNum}
             pgPgSize={pgPgSize}
             setKeyword={setKeyword}
+            sort={sort}
+            setSort={setSort}
           />
         )
       }
@@ -475,6 +497,10 @@ const ListMode = ({
   pgPgNum,
   pgPgSize,
   setKeyword,
+  sort,
+  setSort,
+  sortCta,
+  setSortCta,
 }) => {
   /******************************************* 
     [ 전달변수 ] - 2~5까지 4개는 페이징전달변수
@@ -483,6 +509,13 @@ const ListMode = ({
     3. unitSize : 게시판 리스트 당 레코드 개수
     4. pageNum : 현재 페이지번호
     5. setPageNum : 현재 페이지번호 변경 메서드
+    6. pgPgNum : 페이지번호
+    7. pgPgSize : 페이징의 페이지 크기
+    8. setKeyword : 검색어
+    9. sort : 정렬기준
+    10. setSort : 정렬기준셋팅
+    11. sortCta : 정렬항목
+    12. setSortCta : 정렬항목셋팅
   *******************************************/
 
   // 코드리턴구역 //////////////////////
@@ -494,9 +527,16 @@ const ListMode = ({
           <option value="cont">Contents</option>
           <option value="unm">Writer</option>
         </select>
-        <select name="sel" id="sel" className="sel">
-          <option value="0">Descending</option>
-          <option value="1">Ascending</option>
+        
+        <select name="sel" id="sel" className="sel"
+        onChange={()=>setSort(sort*-1)}
+        >
+          <option value="0" 
+          selected={sort==1?true:false}>
+            Descending</option>
+          <option value="1"
+          selected={sort==-1?true:false}>
+            Ascending</option>
         </select>
         <input id="stxt" type="text" maxLength="50" />
         <button
@@ -526,6 +566,15 @@ const ListMode = ({
         >
           Search
         </button>
+        
+        {/* 정렬기준선택박스 */}
+        <select name="sort_cta" id="sort_cta" className="sort_cta"
+        style={{float:"right",translate:"0 5px"}}>
+          <option value="idx" 
+          selected={sortCta=="idx"?true:false}>Recent</option>
+          <option value="tit" 
+          selected={sortCta=="tit"?true:false}>Title</option>
+        </select>
       </div>
       <table className="dtbl" id="board">
         <thead>
