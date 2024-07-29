108bc4fdd545daff7799a24ed45dbef0d29882f0
Tom B. Parker <tombap8@hotmail.com>
2024-07-29 18:11:42 +0900

    Update index.js

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, useLocation, HashRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  HashRouter,
+} from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Character from "./components/pages/Character";
 import Movies from "./components/pages/Movies";
@@ -57,19 +63,15 @@ import Login from "./components/pages/Login";
 export default function MainComponent() {
   return (
     // 라우터 루트로 라우터 구성시작
-    // basename 속성은 package.json의 "homepage"속성값을 
+    // basename 속성은 package.json의 "homepage"속성값을
     // 읽어옴 (읽는 방법은 process.env.PUBLIC_URL)
-    // <BrowserRouter basename={process.env.PUBLIC_URL}>
-<HashRouter>
-  {/* basename속성을 쓰지 않는다. 
-  해쉬라우터는 homepage 속성값을 자동으로 연결해준다! */}
-
-
-
-
+    // <HashRouter>
+    // basename속성을 쓰지 않는다.
+    // 해쉬라우터는 homepage 속성값을 자동으로 연결해준다!
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       {/* 라우터 경로 변경시 최상단이동 컴포넌트 */}
       <ScrollTop />
-      
+
       <Routes>
         {/* 중요!!! 레이아웃 컴포넌트를 루트로 설정!
         루트 Route 는 홀로닫지말고 반드시 다른
@@ -95,8 +97,8 @@ export default function MainComponent() {
         </Route>
         {/* Layout 루트 Route로 하위 Route를 감싼다! */}
       </Routes>
-</HashRouter>
-    // </BrowserRouter>
+    </BrowserRouter>
+    // </HashRouter>
   );
 }
 
@@ -105,24 +107,22 @@ export default function MainComponent() {
   라우터 경로변경시 스크롤 최상단이동
 *******************************************/
 const ScrollTop = () => {
-
   // 라우터 경로 변경시 path 값 읽어오기
   // pathname 객체 속성에 담긴다!
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
 
   // 화면랜더링 구역에 스크롤상단이동 코드넣기
-  useEffect(()=>{
+  useEffect(() => {
     // 스크롤 최상단 이동
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
     // 변경된 라우터 경로값 확인
     // console.log("라우터경로:",pathname);
-  },[pathname]);
+  }, [pathname]);
   // 의존성을 라우터 경로 변수로 설정한다!
 
   // 컴포넌트 리턴이 필요하나
   // 소스리턴이 아니므로 null를 쓴다
   return null;
-
 }; /////////// ScrollTop 컴포넌트 ////////////
 
 /// 컴포넌트 출력 ///
