ae8dc1bca942a4e1d440a40ed1c8ffc63747b223
Tom B. Parker <tombap8@hotmail.com>
2024-08-05 14:09:40 +0900

    ss

---
+++ 013.SQL학습/오늘의쿼리_2408050.sql
@@ -0,0 +1,12 @@
+-- 주석
+-- DB는 크게 2가지 DDL / DML 이 있다!
+-- DML은 크루드! CRUD -> Create(Insert)/Read(Select)/Update/Delete
+-- [ select 선택항목 from 테이블명 where 조건절 ]
+-- SELECT * FROM Customers;
+-- SELECT city FROM Customers;
+-- SELECT ContactName,city FROM Customers;
+-- SELECT ContactName,city FROM Customers where city='Madrid';
+-- like 검색하기 : %있는 부분이 자유로운 부분임!
+-- select * from Customers where city like "a%"
+-- select * from Customers where city like "%co%"
+select * from Customers where city like "%n"
