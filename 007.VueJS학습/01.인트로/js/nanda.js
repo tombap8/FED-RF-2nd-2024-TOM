15b7d4e3264aa2131d3f0979533bc2cae7c06074
Tom B. Parker <tombap8@hotmail.com>
2024-08-06 10:19:37 +0900

    ss

--- 007.VueJS학습/01.인트로/js/nanda.js
+++ 007.VueJS학습/01.인트로/js/nanda.js
@@ -12,7 +12,15 @@ const vm = new Vue({
         // 2-1. 샵명 데이터
         bigTit: "Style NANDA",
         // 2-2. 로고 태그정보
-        logo: `<img src="./images/logo_3ce.png" alt="nanda logo">`,
+        logo: `<img 
+        src="./images/logo_3ce.png" 
+        alt="nanda logo">`,
+        // 2-3. 배너 데이터
+        content: `
+            나는 날고 싶어~!
+            <h2>오늘도 당신은 날고 싶다~!</h2>
+            <img src="./images/sub_banner_e.jpg" alt="banner">
+        `,
     },
 
 }); /////// Vue ///////////////////
