feeb9139765f7f097625df1a251592d796aa6b58
Tom B. Parker <tombap8@hotmail.com>
2024-08-09 10:23:14 +0900

    Update Board.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -84,6 +84,26 @@ export default function Board() {
   // 페이징의 페이징 개수 : 한번에 보여줄 페이징개수
   const pgPgSize = 3;
 
+  // [ 리듀서함수에서 쓸 리턴값 만들기 함수 ] ///
+  const retVal = (gval,txt) => {
+    return(
+      // 1. 별구분자가 있는가?
+      gval.indexOf("*") !== -1
+      // 2. true면 split으로 잘라서 배열값 검사하기
+      ? gval.split("*").includes(txt)
+      // 2-1. 배열값에 있으면 true이므로 gval추가안함
+      ? gval
+      // 2-2. false면 gval에 현재값 별 넣고 추가
+      : gval + (gval != "" ? "*" : "") + txt
+      // 3. 전체 false이면 빈값이 아니면 문자열검사하기
+      : gval == txt
+      // 3-1. 값이 서로 같으면 추가하지 말기
+      ? gval 
+      // 3-2. 그밖의 경우엔 추가하기
+      : gval + (gval != "" ? "*" : "") + txt
+    );
+  }; ////// retVal함수 ///////////////
+
   // 검색 기능을 위한 리듀서 함수 ////
   const reducerFn = (gval, action) => {
     // gval - 지가 벨류레...의 줄임말...
@@ -121,19 +141,7 @@ export default function Board() {
           alert("Please enter a keyword!");
         }
         // 리턴코드값은 리듀서 변수에 할당!
-        return (
-          // 숙제: *문자열이 있으면 split으로 잘라서
-          // 배열로 만들고 배열값중 현재 입력된 txt가
-          // 배열중에 없으면 새로 등록하고 있으면
-          // 등록하지 않는다를 코드로 작성할것!
-          // 힌트1: 등록않는다는 gval만 넣으면 됨
-          // 힌트2: 배열값 중 단순비교는 includes()사용!
-          gval.indexOf("*") !== -1
-            ? gval.split("*").includes(txt)
-              ? gval
-              : gval + (gval != "" ? "*" : "") + txt
-            : gval + (gval != "" ? "*" : "") + txt
-        );
+        return retVal(gval,txt);
       }
       // (2) 전체리스트 돌아기기 실행코드
       case "back":
@@ -178,21 +186,8 @@ export default function Board() {
         else {
           alert("Please enter a keyword!");
         }
-        // 리턴코드값은 리듀서 변수에 할당!
-        // 리턴코드값은 리듀서 변수에 할당!
-        return (
-          // 숙제: *문자열이 있으면 split으로 잘라서
-          // 배열로 만들고 배열값중 현재 입력된 txt가
-          // 배열중에 없으면 새로 등록하고 있으면
-          // 등록하지 않는다를 코드로 작성할것!
-          // 힌트1: 등록않는다는 gval만 넣으면 됨
-          // 힌트2: 배열값 중 단순비교는 includes()사용!
-          gval.indexOf("*") !== -1
-            ? gval.split("*").includes(txt)
-              ? gval
-              : gval + (gval != "" ? "*" : "") + txt
-            : gval + (gval != "" ? "*" : "") + txt
-        );
+        // 리턴코드값은 리듀서 변수에 할당!        
+        return retVal(gval,txt);
       }
     }
   };
