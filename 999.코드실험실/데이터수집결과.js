833464c33da4f33ec4a72a6cf9150b7ae525d8c7
Tom B. Parker <tombap8@hotmail.com>
2024-12-27 16:22:33 +0900

    ss

---
+++ 999.코드실험실/데이터수집결과.js
@@ -0,0 +1,42 @@
+[
+  {
+    idx: 1,
+    title: "부츠컷 리부트! 이번 시즌 데님 트렌드 부츠컷 청바지 입는 법",
+    imgSrc:
+      "https://img.vogue.co.kr/vogue/2024/04/style_6630ab4f984ce-500x500.jpg?x=1196&y=1123",
+    catName: "패션 아이템",
+    date: "2024.04.30by 황혜원, Alice Cary, Joy Montgomery",
+  },
+  {
+    idx: 2,
+    title: "이번 일요일에 입고 싶은 다코타 존슨의 흰색 청바지와 스니커즈 조합",
+    imgSrc:
+      "https://img.vogue.co.kr/vogue/2024/03/style_65f029d07cfbb-500x500.jpg?x=1106&y=1073",
+    catName: "셀러브리티 스타일",
+    date: "2024.03.13by 황혜원",
+  },
+  {
+    idx: 3,
+    title: "청바지에 샌들로 완벽하게 청순해진 앤 해서웨이의 스프링 룩",
+    imgSrc:
+      "https://img.vogue.co.kr/vogue/2024/03/style_66013041472df-500x500.jpg?x=1634&y=1435",
+    catName: "셀러브리티 스타일",
+    date: "2024.03.25by 황혜원",
+  },
+  {
+    idx: 4,
+    title: "지젤 번천을 더욱 길고 늘씬하게 만들어준 청바지",
+    imgSrc:
+      "https://img.vogue.co.kr/vogue/2024/03/style_65efc366d5b53-500x500.jpg?x=1522&y=1451",
+    catName: "Fashion",
+    date: "2024.03.12by 황혜원, Héloïse Salessy",
+  },
+  {
+    idx: 5,
+    title: "가을에도 청순할 수 있어요",
+    imgSrc:
+      "https://img.vogue.co.kr/vogue/2023/09/style_64f82c51bd9e7-500x500.jpg?x=3680&y=4950",
+    catName: "셀러브리티 스타일",
+    date: "2023.09.07by 황혜원",
+  },
+];
