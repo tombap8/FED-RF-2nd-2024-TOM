8c42bc8e810c8d4181c03a448ce2276b3cfd33bc
Tom B. Parker <tombap8@hotmail.com>
2025-02-06 09:45:21 +0900

    Update vuex_store.js

--- 701.쉐도잉나우25/02.VOGUE_PJ/03.구현소스/js/vuex_store.js
+++ 701.쉐도잉나우25/02.VOGUE_PJ/03.구현소스/js/vuex_store.js
@@ -4,20 +4,13 @@
 // [뷰엑스 스토어 인스턴스를 생성한다!!!] //////
 
 // export const store = new Vuex.Store({
-    export default new Vuex.Store({
-        // 1. 데이터 셋팅구역
-        state: {
-
-        },
-        // 2. 데이터 변경 메서드 구역
-        // -> 컴포넌트에서 호출시 commit() 사용!
-        mutations: {
-
-        },
-        // 3. 비동기처리 메서드 구역
-        // -> 컴포넌트에서 호출시 dispatch() 사용!
-        actions: {
-            
-        },
-      }); /////////// Vuex.Store ///////////////
-      
+export default new Vuex.Store({
+  // 1. 데이터 셋팅구역
+  state: {},
+  // 2. 데이터 변경 메서드 구역
+  // -> 컴포넌트에서 호출시 commit() 사용!
+  mutations: {},
+  // 3. 비동기처리 메서드 구역
+  // -> 컴포넌트에서 호출시 dispatch() 사용!
+  actions: {},
+}); /////////// Vuex.Store ///////////////
