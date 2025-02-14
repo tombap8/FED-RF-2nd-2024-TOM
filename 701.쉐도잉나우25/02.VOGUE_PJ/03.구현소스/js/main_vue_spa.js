e89216606e8f6960c908714fe3feca29fc341db8
Tom B. Parker <tombap8@hotmail.com>
2025-02-14 17:42:57 +0900

    ss

--- 701.쉐도잉나우25/02.VOGUE_PJ/03.구현소스/js/main_vue_spa.js
+++ 701.쉐도잉나우25/02.VOGUE_PJ/03.구현소스/js/main_vue_spa.js
@@ -9,46 +9,53 @@ import store from "./vuex_store.js";
     메인 뷰 인스턴스 생성하기 
 ********************************/
 new Vue({
-    // 1. 대상설정
-    el: '#vogue-app',
-    // 1.5. 라우터등록!
-    router,
-    // 1.6. 스토어등록!
-    store,
-    // 2. 데이터설정
-    data:{},
-    // 3. 메서드설정
-    methods:{
-        // 최초 로그인 확인하기
-        // 로그인 상태라면 세션스토리지값을 읽어옴
-        // 왜? 로컬스가 아닌 세션스인가?
-        // 로그인 상태는 브라우저가 떠 있을동안만 필요!
-        initSet(){
-            let temp;
-            // 세션스 'login-user'가 있으면 처리!
-            if(sessionStorage.getItem('login-user')){
-                // 세션스값 객체변환
-               temp = JSON.parse(sessionStorage.getItem('login-user'));
-               // 뮤테이션 로그인 셋 메서드 호출
-               store.commit('setLogin',temp);
-            }
-        },
+  // 1. 대상설정
+  el: "#vogue-app",
+  // 1.5. 라우터등록!
+  router,
+  // 1.6. 스토어등록!
+  store,
+  // 2. 데이터설정
+  data: {},
+  // 3. 메서드설정
+  methods: {
+    // 최초 로그인 확인하기
+    // 로그인 상태라면 세션스토리지값을 읽어옴
+    // 왜? 로컬스가 아닌 세션스인가?
+    // 로그인 상태는 브라우저가 떠 있을동안만 필요!
+    initSet() {
+      let temp;
+      // 세션스 'login-user'가 있으면 처리!
+      if (sessionStorage.getItem("login-user")) {
+        // 세션스값 객체변환
+        temp = JSON.parse(sessionStorage.getItem("login-user"));
+        // 뮤테이션 로그인 셋 메서드 호출
+        store.commit("setLogin", temp);
+      }
     },
-    // 4. 라이프사이클 메서드
-    // 4-1. created() : 데이터생성관련코드 작성
-    created(){
-        // 최초 로그인 상태 셋팅메서드 호출
-        this.initSet();
+    checkPage() {
+      if (this.$route.path == "/") $(".top-area").removeClass("sub");
+      else $(".top-area").addClass("sub");
     },
-    // 4-2. 업데이트시 실행구역
-    updated(){
-        console.log('메인, 업데이트!');
-    },
+  },
+  // 4. 라이프사이클 메서드
+  // 4-1. created() : 데이터생성관련코드 작성
+  created() {
+    // 최초 로그인 상태 셋팅메서드 호출
+    this.initSet();
+  },
+  // 4-2. 업데이트시 실행구역
+  updated() {
+    console.log("메인, 업데이트!", this.$route.path);
+    this.checkPage();
+  },
 
-    // 4-3. mounted() : DOM관련코드 작성
-    mounted(){
-        // 만약 첫페이지가 다른 경로면
-        // DOM로딩후 구역에서 라우터를 강제로 호출함!
-        // this.$router.push('/');
-    },
+  // 4-3. mounted() : DOM관련코드 작성
+  mounted() {
+    // 만약 첫페이지가 다른 경로면
+    // DOM로딩후 구역에서 라우터를 강제로 호출함!
+    // this.$router.push('/');
+
+    this.checkPage();
+  },
 });
