0519bcfa24b0a79f0c16c859c58ed76b25a5a27d
Tom B. Parker <tombap8@hotmail.com>
2025-04-24 14:31:39 +0900

    ss

--- 9995.FireBase/fire-base-app/src/index.js
+++ 9995.FireBase/fire-base-app/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 // 사스(Sass)팩키지를 설치했다면 바로 사스사용가능
 // 확장자를 여기서는 쓰고 사스파일 import에선 생략가능
 import "./css/main.scss";
-import UsersForm from './components/UsersForm';
+import UserFormList from './components/UserFormList';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root'));
 root.render(
   <div>
       <h1 style={{whiteSpace:"nowrap"}}>🚀 Firebase + React</h1>
-      <UsersForm />
+      <UserFormList />
     </div>
 );
